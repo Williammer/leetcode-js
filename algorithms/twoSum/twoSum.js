6f42fc968af3c07f2a4d07c19daf9c34e39c1207
Williammer <supervegeter@gmail.com>
2016-06-26 11:48:39 +0800

    Added Hash solution to twoSum

--- algorithms/twoSum/twoSum.js
+++ algorithms/twoSum/twoSum.js
@@ -15,7 +15,9 @@
 var twoSum = {};
 /*
  * Solution 1: brutal force loop
- * O(n^2)
+ *
+ * Time complexity: O(N^2)
+ * Space complexity: O(1)
  */
 twoSum.brutal = function(nums, target) {
     if (!(Array.isArray(nums) && nums.length > 1) || typeof target !== 'number') {
@@ -33,6 +35,35 @@ twoSum.brutal = function(nums, target) {
                 return [i, j];
             }
         }
+    }
+    return;
+};
+
+/*
+ * Solution 2: Hash
+ * @Key: nums.value
+ * @Value: nums.key
+ *
+ * Time complexity:  O(N)
+ * Space complexity: O(N)
+ */
+twoSum.hash = function(nums, target) {
+    if (!(Array.isArray(nums) && nums.length > 1) || typeof target !== 'number') {
+        return;
+    }
+
+    var i, hash = {}, key,
+        len = nums.length,
+        compensate;
+
+    for (i = 0; i < len; i++) {
+        compensate = target - nums[i];
+
+        key = hash[compensate];
+        if (typeof key === 'number' && key !== i) {
+            return [key, i];
+        }
+        hash[nums[i]] = i;
     }
     return;
 };
