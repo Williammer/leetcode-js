ccf61c99629e192875b6ee3781142976e7e3181e
Williammer <supervegeter@gmail.com>
2016-06-26 11:50:38 +0800

    Use compact style java hash solution.

--- algorithms/twoSum/twoSum.java
+++ algorithms/twoSum/twoSum.java
@@ -5,16 +5,14 @@
  * @param {number} target
  * @return {number[]}
  */
-public int[] twoSum(int[] numbers, int target) {
-    int[] result = new int[2];
-    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-    for (int i = 0; i < numbers.length; i++) {
-        if (map.containsKey(target - numbers[i])) {
-            result[1] = i + 1;
-            result[0] = map.get(target - numbers[i]);
-            return result;
+public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) {
+            return new int[] { map.get(complement), i };
         }
-        map.put(numbers[i], i + 1);
+        map.put(nums[i], i);
     }
-    return result;
+    throw new IllegalArgumentException("No two sum solution");
 }
