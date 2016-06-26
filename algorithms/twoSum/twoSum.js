04528429ed25cb13a14b466f8d8ef3337756dd95
Williammer <supervegeter@gmail.com>
2016-06-26 12:26:11 +0800

    fix

--- algorithms/twoSum/twoSum.js
+++ algorithms/twoSum/twoSum.js
@@ -4,7 +4,7 @@
    You may assume that each input would have exactly one solution.
    The return format had been changed to zero-based indices.
 
-   Example:
+ * Example:
    Given nums = [2, 7, 11, 15], target = 9,
    Because nums[0] + nums[1] = 2 + 7 = 9,
    return [0, 1].
