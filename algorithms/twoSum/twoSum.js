9b151851c013509235cd9b356b786dab14185476
Williammer <supervegeter@gmail.com>
2016-06-26 12:16:45 +0800

    updated comment

--- algorithms/twoSum/twoSum.js
+++ algorithms/twoSum/twoSum.js
@@ -1,19 +1,23 @@
-/*****
+/************************************************************
  * Problem: https://leetcode.com/problems/two-sum/
- * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
- * You may assume that each input would have exactly one solution.
- * The return format had been changed to zero-based indices.
- *
+   Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+   You may assume that each input would have exactly one solution.
+   The return format had been changed to zero-based indices.
+
+   Example:
+   Given nums = [2, 7, 11, 15], target = 9,
+   Because nums[0] + nums[1] = 2 + 7 = 9,
+   return [0, 1].
+
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
- *
- *
+
+
  * Analysis: requirement is mainly on getting 'key' from valid 'value',
- * which makes Hash to be an efficient data structure to use.
- *
- *
- *****/
+   which makes Hash to be an efficient data structure to use.
+
+************************************************************/
 
 var twoSum = {};
 /*
@@ -75,11 +79,11 @@ twoSum.hash = function(nums, target) {
 return twoSum;
 
 
-/*****
+/************************************************************
  * Lessons:
- * 1. Consider introduce appriorate helper data structures to solve problems.
- *
- * 2. Key, Value are just relative and can be exchanged when needed.
- * For js, with the ES6 Map/WeakMap that allows all types to be 'key', this trick can be applied easier.
- *
- *****/
+   1. Consider introduce appriorate helper data structures to solve problems.
+
+   2. Key, Value are just relative and can be exchanged when needed.
+   For js, with the ES6 Map/WeakMap that allows all types to be 'key', this trick can be applied easier.
+
+************************************************************/
