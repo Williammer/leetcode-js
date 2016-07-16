8be1225d6853e8c38905cf70aa7128b59bd5d0ef
WilliamHe <william.he@accedo.tv>
2016-07-16 10:39:19 +0800

    replaced old es5 spec with es6 spec.

--- test/spec/2.add2Nums.spec.js
+++ test/spec/2.add2Nums.spec.js
@@ -14,18 +14,15 @@
       this.next = null;
     }
 ************************************************************/
+import { ListNode, arrToLList, lListToArr, add2Nums } from "../../algorithms/es6/2.add2Nums/solution";
 
 describe('# Problem 2 - Add two non-negative numbers represented by two linked lists into a number represented by a linked list too', function() {
-    beforeEach(function() {
-        var l1, l2, result;
-    });
-
     // # Helper tests
     describe('Helper method tests', function() {
         it('arrToLList return (2 -> 4 -> 3) for [2,4,3]', function() {
             var testArr = [2, 4, 3];
 
-            result = add2Nums.arrToLList(testArr);
+            var result = arrToLList(testArr);
             expect(result instanceof ListNode).toBeTruthy();
             expect(result.val).toEqual(2);
             expect(result.next.val).toEqual(4);
@@ -37,7 +34,7 @@ describe('# Problem 2 - Add two non-negative numbers represented by two linked l
             testlList.next = new ListNode(4);
             testlList.next.next = new ListNode(3);
 
-            result = add2Nums.lListToArr(testlList);
+            var result = lListToArr(testlList);
             expect(result).toEqual([2, 4, 3]);
         });
     });
@@ -47,39 +44,39 @@ describe('# Problem 2 - Add two non-negative numbers represented by two linked l
     describe('Solution 1: Intro a stack helper to reversely insert each node into linklist', function() {
         it('return right sum for 2 nums with same digits num that has not carry num: [2, 4, 3]+[5, 2, 5] => [7, 6, 8]', function() {
             var l1, l2, result;
-            l1 = add2Nums.arrToLList([2, 4, 3]);
-            l2 = add2Nums.arrToLList([5, 2, 5]);
+            l1 = arrToLList([2, 4, 3]);
+            l2 = arrToLList([5, 2, 5]);
 
-            result = add2Nums.stackHelper(l1, l2);
-            expect(add2Nums.lListToArr(result)).toEqual([7, 6, 8]);
+            var result = add2Nums.stackHelper(l1, l2);
+            expect(lListToArr(result)).toEqual([7, 6, 8]);
 
         });
 
         it('return right sum for 2 nums with same digits num that has carry num: [2, 4, 3]+[5, 6, 4] => [7, 0, 8]', function() {
             var l1, l2, result;
-            l1 = add2Nums.arrToLList([2, 4, 3]);
-            l2 = add2Nums.arrToLList([5, 6, 4]);
+            l1 = arrToLList([2, 4, 3]);
+            l2 = arrToLList([5, 6, 4]);
 
-            result = add2Nums.stackHelper(l1, l2);
-            expect(add2Nums.lListToArr(result)).toEqual([7, 0, 8]);
+            var result = add2Nums.stackHelper(l1, l2);
+            expect(lListToArr(result)).toEqual([7, 0, 8]);
         });
 
         it('return sum should exceeds max input digit num limit with carry num: [2, 4, 9]+[5, 9] => [7, 3, 0, 1]', function() {
             var l1, l2, result;
-            l1 = add2Nums.arrToLList([2, 4, 9]);
-            l2 = add2Nums.arrToLList([5, 9]);
+            l1 = arrToLList([2, 4, 9]);
+            l2 = arrToLList([5, 9]);
 
-            result = add2Nums.stackHelper(l1, l2);
-            expect(add2Nums.lListToArr(result)).toEqual([7, 3, 0, 1]);
+            var result = add2Nums.stackHelper(l1, l2);
+            expect(lListToArr(result)).toEqual([7, 3, 0, 1]);
         });
 
         it('return right sum for 2 nums with different digits num: [6, 9, 3]+[9] => [5, 0, 4]', function() {
             var l1, l2, result;
-            l1 = add2Nums.arrToLList([6, 9, 3]);
-            l2 = add2Nums.arrToLList([9]);
+            l1 = arrToLList([6, 9, 3]);
+            l2 = arrToLList([9]);
 
-            result = add2Nums.stackHelper(l1, l2);
-            expect(add2Nums.lListToArr(result)).toEqual([5, 0, 4]);
+            var result = add2Nums.stackHelper(l1, l2);
+            expect(lListToArr(result)).toEqual([5, 0, 4]);
         });
 
     });
@@ -89,39 +86,39 @@ describe('# Problem 2 - Add two non-negative numbers represented by two linked l
     describe('Solution 2: js reference. Utilized js object reference change feature', function() {
         it('return right sum for 2 nums with same digits num that has not carry num: [2, 4, 3]+[5, 2, 5] => [7, 6, 8]', function() {
             var l1, l2, result;
-            l1 = add2Nums.arrToLList([2, 4, 3]);
-            l2 = add2Nums.arrToLList([5, 2, 5]);
+            l1 = arrToLList([2, 4, 3]);
+            l2 = arrToLList([5, 2, 5]);
 
-            result = add2Nums.reference(l1, l2);
-            expect(add2Nums.lListToArr(result)).toEqual([7, 6, 8]);
+            var result = add2Nums.reference(l1, l2);
+            expect(lListToArr(result)).toEqual([7, 6, 8]);
 
         });
 
         it('return right sum for 2 nums with same digits num that has carry num: [2, 4, 3]+[5, 6, 4] => [7, 0, 8]', function() {
             var l1, l2, result;
-            l1 = add2Nums.arrToLList([2, 4, 3]);
-            l2 = add2Nums.arrToLList([5, 6, 4]);
+            l1 = arrToLList([2, 4, 3]);
+            l2 = arrToLList([5, 6, 4]);
 
-            result = add2Nums.reference(l1, l2);
-            expect(add2Nums.lListToArr(result)).toEqual([7, 0, 8]);
+            var result = add2Nums.reference(l1, l2);
+            expect(lListToArr(result)).toEqual([7, 0, 8]);
         });
 
         it('return sum should exceeds max input digit num limit with carry num: [2, 4, 9]+[5, 9] => [7, 3, 0, 1]', function() {
             var l1, l2, result;
-            l1 = add2Nums.arrToLList([2, 4, 9]);
-            l2 = add2Nums.arrToLList([5, 9]);
+            l1 = arrToLList([2, 4, 9]);
+            l2 = arrToLList([5, 9]);
 
-            result = add2Nums.reference(l1, l2);
-            expect(add2Nums.lListToArr(result)).toEqual([7, 3, 0, 1]);
+            var result = add2Nums.reference(l1, l2);
+            expect(lListToArr(result)).toEqual([7, 3, 0, 1]);
         });
 
         it('return right sum for 2 nums with different digits num: [6, 9, 3]+[9] => [5, 0, 4]', function() {
             var l1, l2, result;
-            l1 = add2Nums.arrToLList([6, 9, 3]);
-            l2 = add2Nums.arrToLList([9]);
+            l1 = arrToLList([6, 9, 3]);
+            l2 = arrToLList([9]);
 
-            result = add2Nums.reference(l1, l2);
-            expect(add2Nums.lListToArr(result)).toEqual([5, 0, 4]);
+            var result = add2Nums.reference(l1, l2);
+            expect(lListToArr(result)).toEqual([5, 0, 4]);
         });
 
     });
@@ -131,39 +128,39 @@ describe('# Problem 2 - Add two non-negative numbers represented by two linked l
     describe('Solution 3: Recursion', function() {
         it('return right sum for 2 nums with same digits num that has not carry num: [2, 4, 3]+[5, 2, 5] => [7, 6, 8]', function() {
             var l1, l2, result;
-            l1 = add2Nums.arrToLList([2, 4, 3]);
-            l2 = add2Nums.arrToLList([5, 2, 5]);
+            l1 = arrToLList([2, 4, 3]);
+            l2 = arrToLList([5, 2, 5]);
 
-            result = add2Nums.recursion(l1, l2);
-            expect(add2Nums.lListToArr(result)).toEqual([7, 6, 8]);
+            var result = add2Nums.recursion(l1, l2);
+            expect(lListToArr(result)).toEqual([7, 6, 8]);
 
         });
 
         it('return right sum for 2 nums with same digits num that has carry num: [2, 4, 3]+[5, 6, 4] => [7, 0, 8]', function() {
             var l1, l2, result;
-            l1 = add2Nums.arrToLList([2, 4, 3]);
-            l2 = add2Nums.arrToLList([5, 6, 4]);
+            l1 = arrToLList([2, 4, 3]);
+            l2 = arrToLList([5, 6, 4]);
 
-            result = add2Nums.recursion(l1, l2);
-            expect(add2Nums.lListToArr(result)).toEqual([7, 0, 8]);
+            var result = add2Nums.recursion(l1, l2);
+            expect(lListToArr(result)).toEqual([7, 0, 8]);
         });
 
         it('return sum should exceeds max input digit num limit with carry num: [2, 4, 9]+[5, 9] => [7, 3, 0, 1]', function() {
             var l1, l2, result;
-            l1 = add2Nums.arrToLList([2, 4, 9]);
-            l2 = add2Nums.arrToLList([5, 9]);
+            l1 = arrToLList([2, 4, 9]);
+            l2 = arrToLList([5, 9]);
 
-            result = add2Nums.recursion(l1, l2);
-            expect(add2Nums.lListToArr(result)).toEqual([7, 3, 0, 1]);
+            var result = add2Nums.recursion(l1, l2);
+            expect(lListToArr(result)).toEqual([7, 3, 0, 1]);
         });
 
         it('return right sum for 2 nums with different digits num: [6, 9, 3]+[9] => [5, 0, 4]', function() {
             var l1, l2, result;
-            l1 = add2Nums.arrToLList([6, 9, 3]);
-            l2 = add2Nums.arrToLList([9]);
+            l1 = arrToLList([6, 9, 3]);
+            l2 = arrToLList([9]);
 
-            result = add2Nums.recursion(l1, l2);
-            expect(add2Nums.lListToArr(result)).toEqual([5, 0, 4]);
+            var result = add2Nums.recursion(l1, l2);
+            expect(lListToArr(result)).toEqual([5, 0, 4]);
         });
 
     });
