d98f86aa7542eeeb55c4d6424a9ef670cbe79384
WilliamHe <william.he@accedo.tv>
2016-07-17 17:01:18 +0800

    Added problem #3

---
+++ algorithms/es5/3.longestSubstr/solution.js
@@ -0,0 +1,141 @@
+/************************************************************
+
+ * Problem: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    Given a string, find the length of the longest substring without repeating characters.
+
+ * Example:
+    Given "abcabcbb", the answer is "abc", which the length is 3.
+    Given "bbbbb", the answer is "b", with the length of 1.
+    Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+ * @param {string} s
+ * @return {number}
+
+ * Analysis: Substring-related problems are classic! In this no-repeated longest substring problem, we can have substr by using sth like 'slide window' with (left, right),
+    and to check the repeatness of each char to be extended into current 'slide window'.
+
+************************************************************/
+
+var longestSubstr = {};
+
+/*
+ * Solution 1: Slide Window, using substring and lastIndexOf to check if repeated.
+ *
+ * "N" is string.length.
+ * Time complexity: O(N^2)
+ * Space complexity: O(1)
+ */
+longestSubstr.slideWin = function(s) {
+    if (!(typeof s === "string" && s.length > 0)) {
+        return 0;
+    }
+
+    var idx = 0,
+        curLen = 1,
+        maxLen = 1,
+        strLen = s.length,
+        isRepeat = false;
+
+    while (idx < strLen - 1 && maxLen < strLen - idx) {
+        if (curLen === 1) {
+            isRepeat = (s[idx] === s[idx + 1]);
+        } else {
+            isRepeat = s.substring(idx, idx+curLen).lastIndexOf(s[idx + curLen]) > -1;
+            // isRepeat = (s.substring(idx, idx+curLen)).includes(s[idx + curLen]); // ES6 str.includes(), similar perf as indexOf/lastIndexOf
+        }
+
+        if (isRepeat) {
+            idx++;
+            curLen = 1;
+        } else {
+            curLen++;
+            if (curLen > maxLen) {
+                maxLen = curLen;
+            }
+        }
+    }
+
+    return maxLen;
+};
+
+/*
+ * Solution 2: Slide Window enhanced, by using lastIndexOf alone, which is super fast!
+ *
+ * "N" is string.length.
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ */
+longestSubstr.slideWinEnhanced = function(s) {
+    var maxLen, left, right, i;
+
+    if (s.length < 2) {
+        return s.length;
+    }
+
+    maxLen = 0;
+
+    for (left = 0, right = 1; right < s.length; right++) {
+        i = s.lastIndexOf(s[right], right - 1);
+        if (i >= 0) { // has repeated
+            maxLen = Math.max(maxLen, right - left);
+            left = Math.max(left, i + 1);
+        }
+    }
+    return Math.max(maxLen, right - left);
+};
+
+
+/*
+ * Solution 3: slide window with hash
+ *
+ * "N" is string.length.
+ * Time complexity: O(N^2)
+ * Space complexity: O(N)
+ */
+longestSubstr.hash = function(s) {
+    var res = {}, i, left = 0, maxLen = 0;
+
+    for (i = 0; i < s.length; i++) {
+        if (res[s[i]] === undefined) {
+            res[s[i]] = i; // {char : idx} pair
+        } else {
+            if (res[s[i]] >= left) {// repeated in current sub str
+                maxLen = Math.max(maxLen, i - left);
+                left = res[s[i]] + 1; // start from repeated char's next char
+            }
+            res[s[i]] = i;
+        }
+    }
+    return Math.max(maxLen, i - left);
+};
+
+/*
+ * Solution 4: hash+reduce. loop string with array.reduce instead of for/while loop, more elegant.
+ *
+ * "N" is nums.length.
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ */
+longestSubstr.hashReduce = function(s) {
+    var map = {}, left = 0;
+
+    return s.split('').reduce((max, v, i) => {
+        left = map[v] >= left ? map[v] + 1 : left;
+        map[v] = i;
+        return Math.max(max, i - left + 1);
+    }, 0);
+};
+
+
+// recursion?
+
+/************************************************************
+
+ * Lessons:
+   1. 'Slide window' is a good perspective for handling sub array/string problems.
+
+   2. Hash is good for checking the existence of chars, and javaScript's lastIndexOf is super fast for this purpose.
+
+   3. ES6's array reduce can loop array elegantly.
+
+************************************************************/
---
+++ test/spec/3.longestSubstr.spec.js
@@ -0,0 +1,155 @@
+/************************************************************
+
+ * Problem: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    Given a string, find the length of the longest substring without repeating characters.
+
+ * Example:
+    Given "abcabcbb", the answer is "abc", which the length is 3.
+    Given "bbbbb", the answer is "b", with the length of 1.
+    Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+ * @param {string} s
+ * @return {number}
+
+ * Analysis: Substring-related problems are classic! In this no-repeated longest substring problem, we can have substr by using sth like 'slide window' with (left, right),
+    and to check the repeatness of each char to be extended into current 'slide window'.
+
+************************************************************/
+
+// import { ListNode, arrToLList, lListToArr, add2Nums } from "../../algorithms/es6/3.longestSubstr/solution";
+
+describe('# Problem 3 - Given a string, find the length of the longest substring without repeating characters.', function() {
+
+    describe('Solution 1: Slide Window, using substring and lastIndexOf to check if repeated', function() {
+        it('return 0 if s has no char: ""', function() {
+            var result = longestSubstr.slideWin("");
+
+            expect(result).toEqual(0);
+        });
+
+        it('return 1 if s has 1 char: "a"', function() {
+            var result = longestSubstr.slideWin("a");
+
+            expect(result).toEqual(1);
+        });
+
+        it('return 2 if s has 2 different char: "ab"', function() {
+            var result = longestSubstr.slideWin("ab");
+
+            expect(result).toEqual(2);
+        });
+
+        it('return expected result for longest substring that not start from the head of string: "anviaj"', function() {
+            var result = longestSubstr.slideWin("anviaj");
+
+            expect(result).toEqual(5);
+        });
+
+        it('return expected result for longest substring that start from the head of string: "abcabcbb"', function() {
+            var result = longestSubstr.slideWin("abcabcbb");
+
+            expect(result).toEqual(3);
+        });
+
+    });
+
+    describe('Solution 2: Slide Window enhanced, by using lastIndexOf alone', function() {
+        it('return 0 if s has no char: ""', function() {
+            var result = longestSubstr.slideWinEnhanced("");
+
+            expect(result).toEqual(0);
+        });
+
+        it('return 1 if s has 1 char: "a"', function() {
+            var result = longestSubstr.slideWinEnhanced("a");
+
+            expect(result).toEqual(1);
+        });
+
+        it('return 2 if s has 2 different char: "ab"', function() {
+            var result = longestSubstr.slideWinEnhanced("ab");
+
+            expect(result).toEqual(2);
+        });
+
+        it('return expected result for longest substring that not start from the head of string: "anviaj"', function() {
+            var result = longestSubstr.slideWinEnhanced("anviaj");
+
+            expect(result).toEqual(5);
+        });
+
+        it('return expected result for longest substring that start from the head of string: "abcabcbb"', function() {
+            var result = longestSubstr.slideWinEnhanced("abcabcbb");
+
+            expect(result).toEqual(3);
+        });
+
+    });
+
+    describe('Solution 3: slide window with hash', function() {
+        it('return 0 if s has no char: ""', function() {
+            var result = longestSubstr.hash("");
+
+            expect(result).toEqual(0);
+        });
+
+        it('return 1 if s has 1 char: "a"', function() {
+            var result = longestSubstr.hash("a");
+
+            expect(result).toEqual(1);
+        });
+
+        it('return 2 if s has 2 different char: "ab"', function() {
+            var result = longestSubstr.hash("ab");
+
+            expect(result).toEqual(2);
+        });
+
+        it('return expected result for longest substring that not start from the head of string: "anviaj"', function() {
+            var result = longestSubstr.hash("anviaj");
+
+            expect(result).toEqual(5);
+        });
+
+        it('return expected result for longest substring that start from the head of string: "abcabcbb"', function() {
+            var result = longestSubstr.hash("abcabcbb");
+
+            expect(result).toEqual(3);
+        });
+
+    });
+
+    describe('Solution 4: hash+reduce. loop string with array.reduce instead of for/while loop, more elegant.', function() {
+        it('return 0 if s has no char: ""', function() {
+            var result = longestSubstr.hashReduce("");
+
+            expect(result).toEqual(0);
+        });
+
+        it('return 1 if s has 1 char: "a"', function() {
+            var result = longestSubstr.hashReduce("a");
+
+            expect(result).toEqual(1);
+        });
+
+        it('return 2 if s has 2 different char: "ab"', function() {
+            var result = longestSubstr.hashReduce("ab");
+
+            expect(result).toEqual(2);
+        });
+
+        it('return expected result for longest substring that not start from the head of string: "anviaj"', function() {
+            var result = longestSubstr.hashReduce("anviaj");
+
+            expect(result).toEqual(5);
+        });
+
+        it('return expected result for longest substring that start from the head of string: "abcabcbb"', function() {
+            var result = longestSubstr.hashReduce("abcabcbb");
+
+            expect(result).toEqual(3);
+        });
+
+    });
+
+});
