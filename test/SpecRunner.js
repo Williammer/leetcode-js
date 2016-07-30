257d0cb89a8c38bd7719cf4bb3b14e76f4efc28d
WilliamHe <william.he@accedo.tv>
2016-07-30 22:43:16 +0800

    Added spec reports for node jasmine.

--- test/SpecRunner.js
+++ test/SpecRunner.js
@@ -1,5 +1,12 @@
 import Jasmine from 'jasmine'
+import SpecReporter from 'jasmine-spec-reporter'
 
-var jasmine = new Jasmine()
-jasmine.loadConfigFile('test/jasmine.config.json')
-jasmine.execute()
+const noop = function() {}
+
+let jrunner = new Jasmine()
+
+jrunner.configureDefaultReporter({print: noop})
+jasmine.getEnv().addReporter(new SpecReporter())
+
+jrunner.loadConfigFile('test/jasmine.config.json')
+jrunner.execute()
