8d6ba33b40a9974b4695b741ce84ecbc630032d5
WilliamHe <william.he@accedo.tv>
2016-10-01 13:37:41 +0800

    Added problem #7.

---
+++ src/7.reverseInt/solution.js
@@ -0,0 +1,46 @@
+/************************************************************
+
+ * Problem: Reverse digits of an integer.
+
+ * Example 1:
+	x = 123, return 321
+ * Example 2:
+	x = -123, return -321
+
+
+ * @param {number} x
+ * @return {number}
+
+ * Analysis: The difficulty for this problem is how to use js to reverse digits of each pos, while keep some edge case in mind.
+
+************************************************************/
+
+
+export const reverseInt = {};
+
+reverseInt.fn = (x) => {
+    const MAX_INT_32 = Math.pow(2, 31) - 1;
+
+    let result = 0,
+        sign = (x >= 0) ? 1 : -1,
+        absX = Math.abs(x);
+
+    while (absX > 0) {
+        result = (absX % 10) + (result * 10); // (absX % 10) is the digits of this pos starting from last one.
+        absX = Math.floor(absX / 10);
+
+        if (result > MAX_INT_32) {
+            return 0;
+        }
+    }
+
+    return sign * result;
+};
+
+
+/************************************************************
+
+ * Lessons:
+   1. "%" and "/" are very useful in javaScript especially in number-related tasks; it's elegant yet powerful, unlike some js operators.
+
+************************************************************/
---
+++ test/spec/7.reverseInt.spec.js
@@ -0,0 +1,51 @@
+import { reverseInt } from "../../src/7.reverseInt/solution";
+
+describe("# Problem 7 - Reverse digits of an integer.", () => {
+
+    describe("Solution 1: use while loop and '%', '/' to reverse digits of each postion.", () => {
+        it("0 -> 0", () => {
+            expect(reverseInt.fn(0)).toEqual(0);
+        });
+
+        it("-123 -> -321", () => {
+            expect(reverseInt.fn(-123)).toEqual(-321);
+        });
+
+        it("-1234 -> -4321", () => {
+            expect(reverseInt.fn(-1234)).toEqual(-4321);
+        });
+
+        it("123 -> 321", () => {
+            expect(reverseInt.fn(123)).toEqual(321);
+        });
+
+        it("1234 -> 4321", () => {
+            expect(reverseInt.fn(1234)).toEqual(4321);
+        });
+
+        it("100 -> 1", () => {
+            expect(reverseInt.fn(100)).toEqual(1);
+        });
+
+        it("-100 -> -1", () => {
+            expect(reverseInt.fn(-100)).toEqual(-1);
+        });
+
+        it("Max value for positive 32bit integer: 7463847412 -> Math.pow(2, 31)-1", () => {
+            expect(reverseInt.fn(7463847412)).toEqual(Math.pow(2, 31)-1);
+        });
+
+        it("Min value for negative 32bit integer: -7463847412 -> -Math.pow(2, 31)+1", () => {
+            expect(reverseInt.fn(-7463847412)).toEqual(-Math.pow(2, 31)+1);
+        });
+
+        it("overflow: 1000000003 -> 0", () => {
+            expect(reverseInt.fn(1000000003)).toEqual(0);
+        });
+
+        it("underflow: -1000000003 -> 0", () => {
+            expect(reverseInt.fn(-1000000003)).toEqual(0);
+        });
+    });
+
+});
