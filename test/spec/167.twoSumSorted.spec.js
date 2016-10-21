fa8f62369596cb7cb1abc54e7b1818d79bc3ceeb
WilliamHe <william.he@accedo.tv>
2016-10-21 10:08:07 +0800

    added a couple test case; reorganized tests

--- test/spec/167.twoSumSorted.spec.js
+++ test/spec/167.twoSumSorted.spec.js
@@ -4,46 +4,6 @@ describe("# Problem 167 - Find 2 indexes of nums from a sorted array which adds 
 
     // # Extended idea: sort then search
     describe("Solution 1: binary search for sorted array.", () => {
-        it("return expected result for only 2 num:  [2, 7] & 9 => [1, 2]", () => {
-            const nums = [2, 7];
-            const target = 9;
-
-            const result = twoSumSorted.bSearch(nums, target);
-            expect(result).toEqual([1, 2]);
-        });
-
-        it("return expected result for even nums array:  [2, 7, 11, 15] & 9 => [1, 2]", () => {
-            const nums = [2, 7, 11, 15];
-            const target = 9;
-
-            const result = twoSumSorted.bSearch(nums, target);
-            expect(result).toEqual([1, 2]);
-        });
-
-        it("return expected result for even nums array:  [1, 2, 7] & 9 => [2, 3]", () => {
-            const nums = [1, 2, 7];
-            const target = 9;
-
-            const result = twoSumSorted.bSearch(nums, target);
-            expect(result).toEqual([2, 3]);
-        });
-
-        it("return expected result for odd nums array:  [2, 7, 11, 15, 20] & 17 => [1, 4]", () => {
-            const nums = [2, 7, 11, 15, 20];
-            const target = 17;
-
-            const result = twoSumSorted.bSearch(nums, target);
-            expect(result).toEqual([1, 4]);
-        });
-
-        it("return expected result for odd nums array:  [1, 7, 11, 15, 20] & 22 => [2, 4]", () => {
-            const nums = [1, 7, 11, 15, 20];
-            const target = 22;
-
-            const result = twoSumSorted.bSearch(nums, target);
-            expect(result).toEqual([2, 4]);
-        });
-
         it("return undefined if no desired result:  [2, 7, 11, 15] & 8 => undefined", () => {
             const nums = [2, 7, 11, 15];
             const target = 8;
@@ -94,6 +54,54 @@ describe("# Problem 167 - Find 2 indexes of nums from a sorted array which adds 
             expect(twoSumSorted.bSearch(nums, much)).toBeUndefined();
             expect(twoSumSorted.bSearch(nums, fn)).toBeUndefined();
             expect(twoSumSorted.bSearch(nums, test)).toBeUndefined();
+        });
+
+        it("return expected result for only 2 num:  [2, 7] & 9 => [1, 2]", () => {
+            const nums = [2, 7];
+            const target = 9;
+
+            const result = twoSumSorted.bSearch(nums, target);
+            expect(result).toEqual([1, 2]);
+        });
+
+        it("return expected result for even nums array:  [2, 7, 11, 15] & 9 => [1, 2]", () => {
+            const nums = [2, 7, 11, 15];
+            const target = 9;
+
+            const result = twoSumSorted.bSearch(nums, target);
+            expect(result).toEqual([1, 2]);
+        });
+
+        it("return expected result for even nums array:  [1, 2, 7] & 9 => [2, 3]", () => {
+            const nums = [1, 2, 7];
+            const target = 9;
+
+            const result = twoSumSorted.bSearch(nums, target);
+            expect(result).toEqual([2, 3]);
+        });
+
+        it("return expected result for odd nums array:  [2, 7, 11, 15, 20] & 17 => [1, 4]", () => {
+            const nums = [2, 7, 11, 15, 20];
+            const target = 17;
+
+            const result = twoSumSorted.bSearch(nums, target);
+            expect(result).toEqual([1, 4]);
+        });
+
+        it("return expected result for odd nums array:  [1, 7, 11, 15, 20] & 22 => [2, 4]", () => {
+            const nums = [1, 7, 11, 15, 20];
+            const target = 22;
+
+            const result = twoSumSorted.bSearch(nums, target);
+            expect(result).toEqual([2, 4]);
+        });
+
+        it("return expected result for odd nums array:  [3,24,50,79,88,150,345] & 200 => [3, 6]", () => {
+            const nums = [3, 24, 50, 79, 88, 150, 345];
+            const target = 200;
+
+            const result = twoSumSorted.bSearch(nums, target);
+            expect(result).toEqual([3, 6]);
         });
     });
 
