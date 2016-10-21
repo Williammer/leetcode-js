c3398602a652be53e367b5d21869d034314e2d54
WilliamHe <william.he@accedo.tv>
2016-10-21 13:29:41 +0800

    Added jasmine node test for single problem.

--- test/SpecRunner.js
+++ test/SpecRunner.js
@@ -1,12 +1,18 @@
-import Jasmine from 'jasmine'
-import SpecReporter from 'jasmine-spec-reporter'
+import Jasmine from 'jasmine';
+import SpecReporter from 'jasmine-spec-reporter';
 
-const noop = function() {}
+const noop = function() {},
+    num = process.argv[2],
+    title = process.argv[3];
 
-let jrunner = new Jasmine()
+let jrunner = new Jasmine();
 
-jrunner.configureDefaultReporter({print: noop})
-jasmine.getEnv().addReporter(new SpecReporter())
+jrunner.configureDefaultReporter({ print: noop });
+jasmine.getEnv().addReporter(new SpecReporter());
 
-jrunner.loadConfigFile('test/jasmine.config.json')
-jrunner.execute()
+if (num || title) {
+    jrunner.execute([`test/spec/${num}.${title}.spec.js`]);
+} else {
+    jrunner.loadConfigFile(`test/jasmine.config.json`);
+    jrunner.execute();
+}
