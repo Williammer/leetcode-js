de21d87c4b9f505823f58a306a0a3fc542349905
WilliamHe <william.he@accedo.tv>
2016-10-21 09:29:02 +0800

    Added problem 167

---
+++ src/167.twoSumSorted/solution.js
@@ -0,0 +1,80 @@
+/************************************************************
+
+ * Problem: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+    Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+	NOTE: index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+ * Example 1:
+    Input: numbers={2, 7, 11, 15}, target=9
+	Output: index1=1, index2=2
+
+
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+
+ * Analysis: Binary Search is the natural choice for sorted array.
+
+************************************************************/
+
+
+export const twoSumSorted = {};
+
+twoSumSorted.bSearch = (sortedNums, target) => {
+    if (!(Array.isArray(sortedNums) && sortedNums.length > 1) || typeof target !== "number") {
+        return;
+    }
+
+    let i, lo, hi, mid, compensate;
+
+    for (i = 0; i < sortedNums.length - 1; i++) {
+        lo = i + 1;
+        hi = sortedNums.length - 1;
+        compensate = target - sortedNums[i];
+
+        // skip in vain searches
+        if (compensate > sortedNums[hi]) {
+            continue;
+        }
+
+        // binary search the compensate target
+        while (lo <= hi) {
+        	// handle 1 or 2 nums on the binarySearch area's case
+            if (lo === hi) {
+            	if (compensate === sortedNums[hi]) {
+                    return [(i + 1), (hi + 1)];
+                } else {
+                    return;
+                }
+            } else if (lo + 1 === hi) {
+                if (compensate === sortedNums[hi]) {
+                    return [(i + 1), (hi + 1)];
+                } else if (compensate === sortedNums[lo]) {
+                    return [(i + 1), (lo + 1)];
+                } else {
+                    return;
+                }
+            }
+
+            // Key is in a[lo..hi] or not present, goes to N/2 if has even nums
+            mid = Math.floor((hi + lo) / 2);
+
+            if (compensate < sortedNums[mid]) {
+                hi = mid - 1;
+            } else if (compensate > sortedNums[mid]) {
+                lo = mid + 1;
+            } else {
+                return [(i + 1), (mid + 1)];
+            }
+        }
+    }
+    return;
+};
+
+
+/************************************************************
+
+ * Lessons:
+   1. Some edge cases of binary search needs to be handled.
+
+************************************************************/
