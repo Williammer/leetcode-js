451c275aa79ef7e11e761e150cbad9329d971770
WilliamHe <william.he@accedo.tv>
2016-10-30 11:08:25 +0800

    Added 102

---
+++ src/102.bTreeLvOrderTraversal/solution.js
@@ -0,0 +1,125 @@
+/************************************************************
+
+ * Problem: https://leetcode.com/problems/binary-tree-level-order-traversal/
+    Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+ * Example 1:
+    Given binary tree [3,9,20,null,null,15,7],
+	    3
+	   / \
+	  9  20
+	    /  \
+	   15   7
+	return its level order traversal as:
+	[
+	  [3],
+	  [9,20],
+	  [15,7]
+	]
+
+
+ * @param {TreeNode} root
+ * @return {number[][]}
+
+ * Analysis: This is similar to the bfs solution in 104.maxDepth problem.
+
+************************************************************/
+
+
+import { TreeNode } from "../_.util/binaryTree";
+
+export const bTreeLvOrderTraversal = {};
+
+/**
+ * Solution 1: Use bfs to traverse.
+ *
+ * "N" is node count
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ */
+bTreeLvOrderTraversal.bfs = (root) => {
+    const isNode = (node) => {
+        return (node instanceof TreeNode) && node.val !== null;
+    };
+
+    if (!root || !isNode(root)) {
+        return [];
+    }
+
+    let result = [],
+        _queue = [];
+
+    _queue.push(root);
+
+    while (_queue.length > 0) {
+        let i = 0,
+            lengthThisDepth = _queue.length,
+            arrayThisDepth = [];
+
+        while (i < lengthThisDepth) {
+            let curNode = _queue.shift();
+            arrayThisDepth.push(curNode.val);
+
+            if (isNode(curNode.left)) {
+                _queue.push(curNode.left);
+            }
+            if (isNode(curNode.right)) {
+                _queue.push(curNode.right);
+            }
+
+            i++;
+        }
+
+        result.push(arrayThisDepth);
+    }
+
+    return result;
+};
+
+/**
+ * Solution 2: Use dfs while keeping the depth state.
+ *
+ * "N" is node count
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ */
+bTreeLvOrderTraversal.dfs = (root) => {
+    const isNode = (node) => {
+        return (node instanceof TreeNode) && node.val !== null;
+    };
+
+    if (!root || !isNode(root)) {
+        return [];
+    }
+
+    let result = [];
+
+    const dfs = (node, depth) => {
+        if (result.length === depth) {
+            // add new depth array when it's on the limit of result array.
+            result.push([]);
+        }
+
+        result[depth].push(node.val);
+
+        if (isNode(node.left)) {
+            dfs(node.left, depth + 1);
+        }
+        if (isNode(node.right)) {
+            dfs(node.right, depth + 1);
+        }
+    };
+
+    dfs(root, 0);
+
+    return result;
+};
+
+
+/************************************************************
+
+ * Lessons:
+	1. level-order traversal is in many ways similar to bfs. One key for bfs is to use a reusable queue to loop over nodes of each depth.
+	2. dfs can also achieve it, just need to keep track of depth state, which corresponds to each result array index - 1.
+
+************************************************************/
---
+++ test/spec/102.bTreeLvOrderTraversal.spec.js
@@ -0,0 +1,129 @@
+import { getBinaryTreeFromArray } from "../../src/_.util/binaryTree";
+import { bTreeLvOrderTraversal } from "../../src/102.bTreeLvOrderTraversal/solution";
+
+describe("# Problem 102 - Given a binary tree, return the level order traversal of its nodes' values..", () => {
+
+    describe("Solution 1: Use bfs to traverse.", () => {
+        it("[] ----> [] ", () => {
+            const array = [];
+            const tree = getBinaryTreeFromArray(array);
+
+            const result = bTreeLvOrderTraversal.bfs(tree);
+            expect(result).toEqual([]);
+        });
+
+        it("[1, 2, 3, null, 4, 5] ----> [[1], [2,3], [4,5]] ", () => {
+            const array = [1, 2, 3, null, 4, 5];
+            const tree = getBinaryTreeFromArray(array);
+
+            const result = bTreeLvOrderTraversal.bfs(tree);
+            expect(result).toEqual([
+                [1],
+                [2, 3],
+                [4, 5]
+            ]);
+        });
+
+        it("[3,9,20,null,null,15,7] ----> [[3],[9, 20],[15, 7]] ", () => {
+            const array = [3, 9, 20, null, null, 15, 7];
+            const tree = getBinaryTreeFromArray(array);
+
+            const result = bTreeLvOrderTraversal.bfs(tree);
+            expect(result).toEqual([
+                [3],
+                [9, 20],
+                [15, 7]
+            ]);
+        });
+
+        it("[1, 2, 3, null, null, 4, 5, 6, 7] ----> [[1], [2,3], [4, 5], [6,7]] ", () => {
+            const array = [1, 2, 3, null, null, 4, 5, 6, 7];
+            const tree = getBinaryTreeFromArray(array);
+
+            const result = bTreeLvOrderTraversal.bfs(tree);
+            expect(result).toEqual([
+                [1],
+                [2, 3],
+                [4, 5],
+                [6, 7]
+            ]);
+        });
+
+        it("[1, 2, 3, null, null, 4, null, 5, 6, 7] ----> [[1], [2,3], [4], [5,6], [7]] ", () => {
+            const array = [1, 2, 3, null, null, 4, null, 5, 6, 7];
+            const tree = getBinaryTreeFromArray(array);
+
+            const result = bTreeLvOrderTraversal.bfs(tree);
+            expect(result).toEqual([
+                [1],
+                [2, 3],
+                [4],
+                [5, 6],
+                [7]
+            ]);
+        });
+    });
+
+
+    describe("Solution 2: Use dfs while keeping the depth state.", () => {
+        it("[] ----> [] ", () => {
+            const array = [];
+            const tree = getBinaryTreeFromArray(array);
+
+            const result = bTreeLvOrderTraversal.dfs(tree);
+            expect(result).toEqual([]);
+        });
+
+        it("[1, 2, 3, null, 4, 5] ----> [[1], [2,3], [4,5]] ", () => {
+            const array = [1, 2, 3, null, 4, 5];
+            const tree = getBinaryTreeFromArray(array);
+
+            const result = bTreeLvOrderTraversal.dfs(tree);
+            expect(result).toEqual([
+                [1],
+                [2, 3],
+                [4, 5]
+            ]);
+        });
+
+        it("[3,9,20,null,null,15,7] ----> [[3],[9, 20],[15, 7]] ", () => {
+            const array = [3, 9, 20, null, null, 15, 7];
+            const tree = getBinaryTreeFromArray(array);
+
+            const result = bTreeLvOrderTraversal.dfs(tree);
+            expect(result).toEqual([
+                [3],
+                [9, 20],
+                [15, 7]
+            ]);
+        });
+
+        it("[1, 2, 3, null, null, 4, 5, 6, 7] ----> [[1], [2,3], [4, 5], [6,7]] ", () => {
+            const array = [1, 2, 3, null, null, 4, 5, 6, 7];
+            const tree = getBinaryTreeFromArray(array);
+
+            const result = bTreeLvOrderTraversal.dfs(tree);
+            expect(result).toEqual([
+                [1],
+                [2, 3],
+                [4, 5],
+                [6, 7]
+            ]);
+        });
+
+        it("[1, 2, 3, null, null, 4, null, 5, 6, 7] ----> [[1], [2,3], [4], [5,6], [7]] ", () => {
+            const array = [1, 2, 3, null, null, 4, null, 5, 6, 7];
+            const tree = getBinaryTreeFromArray(array);
+
+            const result = bTreeLvOrderTraversal.dfs(tree);
+            expect(result).toEqual([
+                [1],
+                [2, 3],
+                [4],
+                [5, 6],
+                [7]
+            ]);
+        });
+    });
+
+});
