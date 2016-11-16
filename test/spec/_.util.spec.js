53c18475148db294af070b0b71448fce28839f65
WilliamHe <william.he@accedo.tv>
2016-11-16 08:37:48 +0800

    Added 1 more spec to getBTreeFromArray.

--- test/spec/_.util.spec.js
+++ test/spec/_.util.spec.js
@@ -67,4 +67,24 @@ describe("[Util] binaryTree - Generate binary tree base on node values array, th
         expect(rootNode.left.right.left.val).toEqual(6);
         expect(rootNode.left.right.right.val).toEqual(7);
     });
+
+    it("[1, 2, 3, 4, null, null, 5, null, null, null, 6]", () => {
+        const array = [1, 2, 3, 4, null, null, 5, null, null, null, 6];
+        const rootNode = getBinaryTreeFromArray(array);
+
+        expect(rootNode.val).toEqual(1);
+
+        expect(rootNode.left.val).toEqual(2);
+        expect(rootNode.right.val).toEqual(3);
+
+        expect(rootNode.left.left.val).toEqual(4);
+        expect(rootNode.left.right).toBeNull();
+        expect(rootNode.right.left).toBeNull();
+        expect(rootNode.right.right.val).toEqual(5);
+
+        expect(rootNode.left.left.left).toBeNull();
+        expect(rootNode.left.left.right).toBeNull();
+        expect(rootNode.right.right.left).toBeNull();
+        expect(rootNode.right.right.right.val).toEqual(6);
+    });
 });
