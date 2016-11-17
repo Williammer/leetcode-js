1e923db976fdbe2124527f6c15c58b6b5dbd569c
WilliamHe <william.he@accedo.tv>
2016-11-18 07:42:32 +0800

    try to add 226

---
+++ test/spec/226.invertBTree.spec.js
@@ -0,0 +1,48 @@
+import { getBinaryTreeFromArray } from "../../src/_.util/binaryTree";
+import { invertBTree } from "../../src/226.invertBTree/solution";
+
+describe("# Problem 226 - .", () => {
+
+    describe("Solution 1: ", () => {
+        it("[] ----> [] ", () => {
+            const array = [];
+            const tree = getBinaryTreeFromArray(array);
+
+            const result = invertBTree.recursion(tree);
+            expect(result).toEqual(0);
+        });
+
+        it("[1] ----> result: 1 ", () => {
+            const array = [1];
+            const tree = getBinaryTreeFromArray(array);
+
+            const result = invertBTree.recursion(tree);
+            expect(result).toEqual(1);
+        });
+
+        it("[1, null, null, 1, 2, 3, 4] ----> result: 1 ", () => {
+            const array = [1, null, null, 1, 2, 3, 4];
+            const tree = getBinaryTreeFromArray(array);
+
+            const result = invertBTree.recursion(tree);
+            expect(result).toEqual(1);
+        });
+
+        it("[1, 2, 3, null, 4, 5] ----> result: 3 ", () => {
+            const array = [1, 2, 3, null, 4, 5];
+            const tree = getBinaryTreeFromArray(array);
+
+            const result = invertBTree.recursion(tree);
+            expect(result).toEqual(3);
+        });
+
+        it("[1, 2, 3, null, 4, 5, null, 6, 7] ----> result: 4 ", () => {
+            const array = [1, 2, 3, null, 4, 5, 6, 7];
+            const tree = getBinaryTreeFromArray(array);
+
+            const result = invertBTree.recursion(tree);
+            expect(result).toEqual(4);
+        });
+    });
+
+});
