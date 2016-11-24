0b1fdcfa6b67fdf9d5950753bb51abfb60aec678
WilliamHe <william.he@accedo.tv>
2016-11-24 08:18:25 +0800

    Added 226.

--- src/226.invertBTree/solution.js
+++ src/226.invertBTree/solution.js
@@ -20,30 +20,66 @@
  * @param {TreeNode} root
  * @return {TreeNode}
 
- * Analysis: 
+ * Analysis: bfs is my first reaction when found the invert is done from top to bottom of the tree.
 
 ************************************************************************************************************************/
 
+import { TreeNode } from "../_.util/binaryTree";
 
 export const invertBTree = {};
 
 /**
- * Solution 1: 
+ * Solution 1: bfs.
  *
- * "N" is 
- * Time complexity: O()
- * Space complexity: O()
+ * "N" is node count
+ * Time complexity: O(N)
+ * Space complexity: O(N)
  */
-invertBTree.recursion = () => {
+invertBTree.bfs = (root) => {
+	const isNode = (node) => {
+        return (node instanceof TreeNode) && node.val !== null;
+    };
+
+    if (!isNode(root)) {
+        return [];
+    }
+
+    let _queue = [root];
+
+    while(_queue.length > 0){
+    	let curLengthThisDepth = _queue.length,
+    		i = 0;
+
+    	while(i < curLengthThisDepth){
+    		let curNode = _queue.shift(),
+    			tmp;
+
+    		if (isNode(curNode.left) || isNode(curNode.right)) {
+    			// invert left, right child of each Node
+    			tmp = curNode.left;
+    			curNode.left = curNode.right;
+    			curNode.right = tmp;
+
+    			if (isNode(curNode.left)) {
+    				_queue.push(curNode.left);
+    			}
+    			if (isNode(curNode.right)) {
+    				_queue.push(curNode.right);
+    			}
+    		}
+
+    		i++;
+    	}
+    }
 
 
-	return;
+	return root;
 };
 
 
 /************************************************************************************************************************
 
  * Lessons:
-   1. 
+   1. Observe the pattern then choose the proper way to solve.
 
 ************************************************************************************************************************/
--- test/spec/226.invertBTree.spec.js
+++ test/spec/226.invertBTree.spec.js
@@ -1,48 +1,58 @@
-import { getBinaryTreeFromArray } from "../../src/_.util/binaryTree";
+import { getBinaryTreeFromArray, getArrayFromBinaryTree } from "../../src/_.util/binaryTree";
 import { invertBTree } from "../../src/226.invertBTree/solution";
 
-describe("# Problem 226 - .", () => {
+describe("# Problem 226 - Invert a binary tree.", () => {
 
-    describe("Solution 1: ", () => {
-        /*it("[] ----> [] ", () => {
+    describe("Solution 1: recursively invert each sub-tree.", () => {
+        it("[] --> []", () => {
             const array = [];
             const tree = getBinaryTreeFromArray(array);
 
-            const result = invertBTree.recursion(tree);
-            expect(result).toEqual(0);
+            const result = invertBTree.bfs(tree);
+            const resultArray = getArrayFromBinaryTree(result);
+
+            expect(resultArray).toEqual([]);
         });
 
-        it("[1] ----> result: 1 ", () => {
+        it("[1] --> [1]", () => {
             const array = [1];
             const tree = getBinaryTreeFromArray(array);
 
-            const result = invertBTree.recursion(tree);
-            expect(result).toEqual(1);
+            const result = invertBTree.bfs(tree);
+            const resultArray = getArrayFromBinaryTree(result);
+
+            expect(resultArray).toEqual([1]);
         });
 
-        it("[1, null, null, 1, 2, 3, 4] ----> result: 1 ", () => {
+        it("[1, null, null, 1, 2, 3, 4] --> [1]", () => {
             const array = [1, null, null, 1, 2, 3, 4];
             const tree = getBinaryTreeFromArray(array);
 
-            const result = invertBTree.recursion(tree);
-            expect(result).toEqual(1);
+            const result = invertBTree.bfs(tree);
+            const resultArray = getArrayFromBinaryTree(result);
+
+            expect(resultArray).toEqual([1]);
         });
 
-        it("[1, 2, 3, null, 4, 5] ----> result: 3 ", () => {
+        it("[1, 2, 3, null, 4, 5] --> [1, 3, 2, null, 5, 4]", () => {
             const array = [1, 2, 3, null, 4, 5];
             const tree = getBinaryTreeFromArray(array);
 
-            const result = invertBTree.recursion(tree);
-            expect(result).toEqual(3);
+            const result = invertBTree.bfs(tree);
+            const resultArray = getArrayFromBinaryTree(result);
+
+            expect(resultArray).toEqual([1, 3, 2, null, 5, 4]);
         });
 
-        it("[1, 2, 3, null, 4, 5, null, 6, 7] ----> result: 4 ", () => {
+        it("[1, 2, 3, null, 4, 5, null, 6, 7] --> [1, 3, 2, 6, 5, 4, null, null, null, null, null, null, 7]", () => {
             const array = [1, 2, 3, null, 4, 5, 6, 7];
             const tree = getBinaryTreeFromArray(array);
 
-            const result = invertBTree.recursion(tree);
-            expect(result).toEqual(4);
-        });*/
+            const result = invertBTree.bfs(tree);
+            const resultArray = getArrayFromBinaryTree(result);
+
+            expect(resultArray).toEqual([1, 3, 2, 6, 5, 4, null, null, null, null, null, null, 7]);
+        });
     });
 
 });
