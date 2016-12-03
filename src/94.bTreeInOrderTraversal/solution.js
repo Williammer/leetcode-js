59b8e6ec9cc083c22424bb3fa819981a5ff37c31
WilliamHe <william.he@accedo.tv>
2016-12-03 21:49:10 +0800

    added 94

---
+++ src/94.bTreeInOrderTraversal/solution.js
@@ -0,0 +1,62 @@
+/************************************************************
+
+ * Problem: https://leetcode.com/problems/binary-tree-inorder-traversal/
+    Given a binary tree, return the inorder traversal of its nodes' values.
+
+ * Example 1:
+        1
+        \
+         2
+        /
+       3
+    return [1,3,2]
+
+ * @param {TreeNode} root
+ * @return {number[]}
+
+ * Analysis: In theory, inorder should still be DFS, so we can try that.
+
+************************************************************/
+
+import { TreeNode } from "../_.util/binaryTree";
+
+export const bTreeInOrderTraversal = {};
+
+/**
+ * Solution 1: use dfs
+ *
+ * "N" is node count
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ */
+bTreeInOrderTraversal.dfs = (root) => {
+    const isNode = (node) => {
+            return (node instanceof TreeNode) && node.val !== null;
+        },
+        result = [],
+        dfsVal = (node) => {
+            if (isNode(node)) {
+                if (isNode(node.left)) {
+                    dfsVal(node.left);
+                }
+
+                result.push(node.val);
+
+                if (isNode(node.right)) {
+                    dfsVal(node.right);
+                }
+            }
+        };
+
+    dfsVal(root);
+
+    return result;
+};
+
+
+/************************************************************
+
+ * Lessons:
+   1. In-order is basically dfs, and dfs is recursion.
+
+************************************************************/
