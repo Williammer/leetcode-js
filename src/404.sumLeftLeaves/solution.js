797a22a2f8934474ff94986ebbc35949204057e2
WilliamHe <william.he@accedo.tv>
2016-12-11 15:55:11 +0800

    Added 404

---
+++ src/404.sumLeftLeaves/solution.js
@@ -0,0 +1,114 @@
+/************************************************************
+
+ * Problem: https://leetcode.com/problems/sum-of-left-leaves/
+    Find the sum of all left leaves in a given binary tree.
+
+ * Example 1:
+    3
+   / \
+  9  20
+    /  \
+   15   7
+There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+
+
+ * @param {TreeNode} root
+ * @return {number}
+
+ * Analysis: Easy to do it with bfs/dfs.
+
+************************************************************/
+
+
+import { TreeNode } from "../_.util/binaryTree";
+
+export const sumLeftLeaves = {};
+
+
+/**
+ * Solution 1: Use dfs traveral to find and sum
+ *
+ * "N" is Node count
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ */
+sumLeftLeaves.dfs = (root) => {
+    const isNode = (node) => {
+            return (node instanceof TreeNode) && node.val !== null;
+        },
+        isLeaf = (node) => {
+            return isNode(node) && !isNode(node.left) && !isNode(node.right);
+        };
+
+    let sum = 0;
+
+    const getSum = (node) => {
+        if (!isNode(node)) {
+            return 0;
+        }
+
+        if (isNode(node.left)) {
+            if (isLeaf(node.left)) {
+                sum += node.left.val;
+            } else {
+                getSum(node.left);
+            }
+        }
+        if (isNode(node.right) && !isLeaf(node.right)) {
+            getSum(node.right);
+        }
+    };
+
+    getSum(root);
+
+    return sum;
+};
+
+/**
+ * Solution 2: Use bfs/level-order traveral to find
+ *
+ * "N" is Node count
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ */
+sumLeftLeaves.bfs = (root) => {
+    const isNode = (node) => {
+            return (node instanceof TreeNode) && node.val !== null;
+        },
+        isLeaf = (node) => {
+            return isNode(node) && !isNode(node.left) && !isNode(node.right);
+        };
+
+    if (!isNode(root)) {
+        return 0;
+    }
+
+    let _queue = [root],
+        sum = 0;
+
+    while (_queue.length > 0) {
+        const curNode = _queue.shift();
+
+        if (isNode(curNode.left)) {
+            if (isLeaf(curNode.left)) {
+                sum += curNode.left.val;
+            } else {
+                _queue.push(curNode.left);
+            }
+        }
+
+        if (isNode(curNode.right) && !isLeaf(curNode.right)) {
+            _queue.push(curNode.right);
+        }
+    }
+
+    return sum;
+};
+
+
+/************************************************************
+
+ * Lessons:
+   1. Yet another bfs/dfs BT problem.
+
+************************************************************/
---
+++ test/spec/404.sumLeftLeaves.spec.js
@@ -0,0 +1,122 @@
+import { arrayToBinaryTree } from "../../src/_.util/binaryTree";
+import { sumLeftLeaves } from "../../src/404.sumLeftLeaves/solution";
+
+describe("# Problem 404 - Find the sum of all left leaves in a given binary tree..", () => {
+
+    describe("Solution 1: Use dfs traversal", () => {
+        it("[] ----> sumOfLeftLeaves: 0 ", () => {
+            const array = [];
+            const tree = arrayToBinaryTree(array);
+
+            const sumOfLeftLeaves = sumLeftLeaves.dfs(tree);
+            expect(sumOfLeftLeaves).toEqual(0);
+        });
+
+        it("[1] ----> sumOfLeftLeaves: 0 ", () => {
+            const array = [0];
+            const tree = arrayToBinaryTree(array);
+
+            const sumOfLeftLeaves = sumLeftLeaves.dfs(tree);
+            expect(sumOfLeftLeaves).toEqual(0);
+        });
+
+        it("[1, null, null, 1, 2, 3, 4] ----> sumOfLeftLeaves: 0 ", () => {
+            const array = [1, null, null, 1, 2, 3, 4];
+            const tree = arrayToBinaryTree(array);
+
+            const sumOfLeftLeaves = sumLeftLeaves.dfs(tree);
+            expect(sumOfLeftLeaves).toEqual(0);
+        });
+
+        it("[1, 2, 3, null, 4, 5] ----> sumOfLeftLeaves: 5 ", () => {
+            const array = [1, 2, 3, null, 4, 5];
+            const tree = arrayToBinaryTree(array);
+
+            const sumOfLeftLeaves = sumLeftLeaves.dfs(tree);
+            expect(sumOfLeftLeaves).toEqual(5);
+        });
+
+        it("[1, 2, 3, null, 4, 5, 6, 7] ----> sumOfLeftLeaves: 12 ", () => {
+            const array = [1, 2, 3, null, 4, 5, 6, 7];
+            const tree = arrayToBinaryTree(array);
+
+            const sumOfLeftLeaves = sumLeftLeaves.dfs(tree);
+            expect(sumOfLeftLeaves).toEqual(12);
+        });
+
+        it("[1, 2, 3, null, 4, 5, null, 6, 7] ----> sumOfLeftLeaves: 11 ", () => {
+            const array = [1, 2, 3, null, 4, 5, null, 6, 7];
+            const tree = arrayToBinaryTree(array);
+
+            const sumOfLeftLeaves = sumLeftLeaves.dfs(tree);
+            expect(sumOfLeftLeaves).toEqual(11);
+        });
+
+        it("[3,9,20,null,null,15,7] ----> sumOfLeftLeaves: 24 ", () => {
+            const array = [3, 9, 20, null, null, 15, 7];
+            const tree = arrayToBinaryTree(array);
+
+            const sumOfLeftLeaves = sumLeftLeaves.dfs(tree);
+            expect(sumOfLeftLeaves).toEqual(24);
+        });
+    });
+
+    describe("Solution 2: Use bfs/level-order traversal", () => {
+        it("[] ----> sumOfLeftLeaves: 0 ", () => {
+            const array = [];
+            const tree = arrayToBinaryTree(array);
+
+            const sumOfLeftLeaves = sumLeftLeaves.bfs(tree);
+            expect(sumOfLeftLeaves).toEqual(0);
+        });
+
+        it("[1] ----> sumOfLeftLeaves: 0 ", () => {
+            const array = [0];
+            const tree = arrayToBinaryTree(array);
+
+            const sumOfLeftLeaves = sumLeftLeaves.bfs(tree);
+            expect(sumOfLeftLeaves).toEqual(0);
+        });
+
+        it("[1, null, null, 1, 2, 3, 4] ----> sumOfLeftLeaves: 0 ", () => {
+            const array = [1, null, null, 1, 2, 3, 4];
+            const tree = arrayToBinaryTree(array);
+
+            const sumOfLeftLeaves = sumLeftLeaves.bfs(tree);
+            expect(sumOfLeftLeaves).toEqual(0);
+        });
+
+        it("[1, 2, 3, null, 4, 5] ----> sumOfLeftLeaves: 5 ", () => {
+            const array = [1, 2, 3, null, 4, 5];
+            const tree = arrayToBinaryTree(array);
+
+            const sumOfLeftLeaves = sumLeftLeaves.bfs(tree);
+            expect(sumOfLeftLeaves).toEqual(5);
+        });
+
+        it("[1, 2, 3, null, 4, 5, 6, 7] ----> sumOfLeftLeaves: 12 ", () => {
+            const array = [1, 2, 3, null, 4, 5, 6, 7];
+            const tree = arrayToBinaryTree(array);
+
+            const sumOfLeftLeaves = sumLeftLeaves.bfs(tree);
+            expect(sumOfLeftLeaves).toEqual(12);
+        });
+
+        it("[1, 2, 3, null, 4, 5, null, 6, 7] ----> sumOfLeftLeaves: 11 ", () => {
+            const array = [1, 2, 3, null, 4, 5, null, 6, 7];
+            const tree = arrayToBinaryTree(array);
+
+            const sumOfLeftLeaves = sumLeftLeaves.bfs(tree);
+            expect(sumOfLeftLeaves).toEqual(11);
+        });
+
+        it("[3,9,20,null,null,15,7] ----> sumOfLeftLeaves: 24 ", () => {
+            const array = [3, 9, 20, null, null, 15, 7];
+            const tree = arrayToBinaryTree(array);
+
+            const sumOfLeftLeaves = sumLeftLeaves.bfs(tree);
+            expect(sumOfLeftLeaves).toEqual(24);
+        });
+    });
+
+});
