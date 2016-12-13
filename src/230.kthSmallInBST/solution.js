d6b6aa3740b340767bee6418e8dd5a2383cb380d
WilliamHe <william.he@accedo.tv>
2016-12-13 08:43:11 +0800

    added solution 2&3 to 230, great solutions.

--- src/230.kthSmallInBST/solution.js
+++ src/230.kthSmallInBST/solution.js
@@ -1,4 +1,4 @@
-/************************************************************
+/************************************************************************************************************************
 
  * Problem: https://leetcode.com/problems/kth-smallest-element-in-a-bst/
     Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
@@ -10,7 +10,7 @@
 
  * Analysis: Try in-order, which traverse nodes in sorted order of values.
 
-************************************************************/
+************************************************************************************************************************/
 
 import { TreeNode } from "../_.util/binaryTree";
 
@@ -62,10 +62,84 @@ kthSmallInBST.inOrder = (root, k) => {
     return result;
 };
 
+/**
+ * Solution 2: use inOrder iterative
+ *
+ * "N" is node count
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ */
+kthSmallInBST.inOrderIter = (root, k) => {
+    const isNode = (node) => {
+        return (node instanceof TreeNode) && node.val !== null;
+    };
 
-/************************************************************
+    if (!isNode(root) || k <= 0) {
+        return undefined;
+    }
+
+    let _stack = [],
+        curNode = root;
+
+    while(isNode(curNode) || _stack.length > 0){
+        // push curNode and each left node to stack until reach left leaf
+        while(isNode(curNode)){
+            _stack.push(curNode);
+            curNode = curNode.left;
+        }
+
+        // process a node in stack
+        curNode = _stack.pop();
+        if(--k === 0){
+            return curNode.val;
+        }
+
+        // refer to the right node of cur popped node
+        curNode = curNode.right;
+    }
+};
+
+/**
+ * Solution 3: use binary search
+ *
+ * "N" is node count
+ * Time complexity: O(logN)
+ * Space complexity: O(logN)
+ */
+kthSmallInBST.binarySearch = (root, k) => {
+    const isNode = (node) => {
+            return (node instanceof TreeNode) && node.val !== null;
+        },
+        nodeCount = (node) => {
+            if (!isNode(node)) {
+                return 0;
+            }
+            return 1 + nodeCount(node.left) + nodeCount(node.right);
+        };
+
+    if (!isNode(root)) {
+        return undefined;
+    }
+
+    let leftCount = nodeCount(root.left);
+    if (k <= leftCount) {
+        // within left sub-tree
+        return kthSmallInBST.binarySearch(root.left, k);
+    } else if (k > leftCount + 1) {
+        // within right sub-tree
+        return kthSmallInBST.binarySearch(root.right, k - leftCount - 1); // start from right sub-tree
+    }
+
+    // within root
+    return root.val;
+};
+
+
+/************************************************************************************************************************
 
  * Lessons:
    1. in-order for BST will traverse the tree in sorted order.
+   2. the iterative in-order traverse is delicated, with using 'while' and 'stack' to full power to fullfill recursion.
+   3. the count of a node & its child is a useful derived data.
 
-************************************************************/
+************************************************************************************************************************/
