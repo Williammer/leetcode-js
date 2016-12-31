a7be0141547b574c08defebda977fac41074a2e9
Williammer <supervegeter@gmail.com>
2016-12-31 16:52:21 +0800

    added 206

---
+++ src/206.reverseLinkedList/solution.js
@@ -0,0 +1,60 @@
+/************************************************************************************************************************
+
+ * Problem: https://leetcode.com/problems/reverse-linked-list/
+    Reverse a singly linked list.
+
+ * Example:
+    Input: (2 -> 4 -> 3)
+    Output: (3 -> 4 -> 2)
+
+
+ * @param {ListNode} head
+ * @return {ListNode}
+
+ * Analysis: Reverse is to break the old listNode's reference and point it to new listNode.
+
+************************************************************************************************************************/
+
+
+import { ListNode, reverseLinkedListFn } from "../_.util/linkedList";
+
+export const reverseLinkedList = {};
+
+/**
+ * Solution 1: normal iterative
+ *
+ * "N" is linkedList length
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ */
+reverseLinkedList.iterate = reverseLinkedListFn;
+
+
+/**
+ * Solution 2: Recursion.
+ *
+ * "N" is linkedList length
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ */
+reverseLinkedList.recursion = (head) => {
+    const reverse = (head, newHead) => {
+        if (!head) {
+            return newHead;
+        }
+        let next = head.next;
+        head.next = newHead;
+
+        return reverse(next, head);
+    };
+
+    return reverse(head, null);
+};
+
+
+/************************************************************************************************************************
+
+ * Lessons:
+   1. LinkedList handling on the code(such as those assignments) is a bit abstract, better think in the visualized way.
+
+************************************************************************************************************************/
--- src/_.util/linkedList.js
+++ src/_.util/linkedList.js
@@ -52,3 +52,27 @@ export const linkedlistToArray = (lList) => {
 
     return arr;
 };
+
+/**
+ * Reverse linkedList
+ *
+ * "N" is item count
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ *
+ * More on 206.reverseLinkedList
+ *
+ */
+export const reverseLinkedListFn = function(head) {
+    let newHead = null,
+        next = null;
+
+    while (head) {
+        next = head.next;
+        head.next = newHead;
+        newHead = head;
+        head = next;
+    }
+
+    return newHead;
+};
