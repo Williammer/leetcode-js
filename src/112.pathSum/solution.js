06bf0fe079b6dd29938ff673e4f7d8afa819b6af
Williammer <supervegeter@gmail.com>
2017-01-01 21:55:47 +0800

    added 112

---
+++ src/112.pathSum/solution.js
@@ -0,0 +1,102 @@
+/************************************************************************************************************************
+
+ * Problem: https://leetcode.com/problems/path-sum/
+    Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+ * Example:
+	Given the below binary tree and sum = 22,
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \      \
+        7    2      1
+	return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+
+ * Analysis: Pre-order dfs can be helpful for finding the path info.
+ 	And since this problem is about root-to-leaf path, we can replace values of each sub-node with root2node value sum.
+
+************************************************************************************************************************/
+
+import { TreeNode } from "../_.util/binaryTree";
+
+export const pathSum = {};
+
+/**
+ * Solution 1: use dfs and reduce node values from root to leaf
+ *
+ * "N" is node count
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ */
+pathSum.dfsReducer = (root, sum) => {
+    const isNode = (node) => {
+            return (node instanceof TreeNode) && node.val !== null;
+        },
+        isLeaf = (node) => {
+            return isNode(node) && !isNode(node.left) && !isNode(node.right);
+        },
+        dfsVal = (node) => {
+            if (isNode(node)) {
+                if (isLeaf(node) && node.val === sum) {
+                    return true;
+                }
+
+                if (isNode(node.left)) {
+                    node.left.val += node.val; // accumulate path sum on each node
+                    if (isLeaf(node.left) && node.left.val === sum || dfsVal(node.left)) {
+                        return true;
+                    }
+                }
+
+                if (isNode(node.right)) {
+                    node.right.val += node.val; // accumulate path sum on each node
+                    if (isLeaf(node.right) && node.right.val === sum || dfsVal(node.right)) {
+                        return true;
+                    }
+                }
+            }
+        };
+
+    return !!dfsVal(root);
+};
+
+/**
+ * Solution 2: use dfs and sum minus which is more concise
+ *
+ * "N" is node count
+ * Time complexity: O(N)
+ * Space complexity: O(N)
+ */
+pathSum.dfs = (root, sum) => {
+    const isNode = (node) => {
+            return (node instanceof TreeNode) && node.val !== null;
+        },
+        isLeaf = (node) => {
+            return isNode(node) && !isNode(node.left) && !isNode(node.right);
+        };
+
+    if (!isNode(root)) {
+        return false;
+    }
+    if (isLeaf(root)) {
+        return root.val === sum;
+    }
+
+    return pathSum.dfs(root.left, sum - root.val) || pathSum.dfs(root.right, sum - root.val);
+};
+
+
+/************************************************************************************************************************
+
+ * Lessons:
+   1. Reduce node values to get path Sum.
+   2. Minus sum to match node values.
+
+************************************************************************************************************************/
---
+++ test/spec/112.pathSum.spec.js
@@ -0,0 +1,245 @@
+import { arrayToBinaryTree } from "../../src/_.util/binaryTree";
+import { pathSum } from "../../src/112.pathSum/solution";
+
+describe("# Problem 112 - Has root2leaf path sum certain.", () => {
+
+    describe("Solution 1: use dfs and reduce node values from root to leaf", () => {
+        it("[], 3 --> false", () => {
+            const array = [];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfsReducer(tree, 3);
+
+            expect(result).toEqual(false);
+        });
+
+        it("[1], 1 --> true", () => {
+            const array = [1];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfsReducer(tree, 1);
+
+            expect(result).toEqual(true);
+        });
+
+        it("[1], 2 --> false", () => {
+            const array = [1];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfsReducer(tree, 2);
+
+            expect(result).toEqual(false);
+        });
+
+        it("[1, null, null, 1, 2, 3, 4], 1 --> true", () => {
+            const array = [1, null, null, 1, 2, 3, 4];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfsReducer(tree, 1);
+
+            expect(result).toEqual(true);
+        });
+
+        it("[1, null, null, 1, 2, 3, 4], 2 --> false", () => {
+            const array = [1, null, null, 1, 2, 3, 4];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfsReducer(tree, 2);
+
+            expect(result).toEqual(false);
+        });
+
+        it("[3, null, 5, 4], 1 --> false", () => {
+            const array = [3, null, 5, 4];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfsReducer(tree, 1);
+
+            expect(result).toEqual(false);
+        });
+
+        it("[3, null, 5, 4], 3 --> false", () => {
+            const array = [3, null, 5, 4];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfsReducer(tree, 3);
+
+            expect(result).toEqual(false);
+        });
+
+        it("[6, null, 8, 7, 9], 23 --> true", () => {
+            const array = [6, null, 8, 7, 9];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfsReducer(tree, 23);
+
+            expect(result).toEqual(true);
+        });
+
+        it("[6, null, 8, 7, 9], 21 --> true", () => {
+            const array = [6, null, 8, 7, 9];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfsReducer(tree, 21);
+
+            expect(result).toEqual(true);
+        });
+
+        it("[6, null, 8, 7, 9], 33 --> false", () => {
+            const array = [6, null, 8, 7, 9];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfsReducer(tree, 33);
+
+            expect(result).toEqual(false);
+        });
+
+        it("[6, 2, 9, null, 4, 7, 10, 3], 15 --> true", () => {
+            const array = [6, 2, 9, null, 4, 7, 10, 3];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfsReducer(tree, 15);
+
+            expect(result).toEqual(true);
+        });
+
+
+        it("[6, 2, 9, null, 4, 7, 10, 3], 37 --> false", () => {
+            const array = [6, 2, 9, null, 4, 7, 10, 3];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfsReducer(tree, 37);
+
+            expect(result).toEqual(false);
+        });
+
+        it("[6, 2, 9, null, 4, 7, 10, 3], 25 --> true", () => {
+            const array = [6, 2, 9, null, 4, 7, 10, 3];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfsReducer(tree, 25);
+
+            expect(result).toEqual(true);
+        });
+    });
+
+    describe("Solution 2: use dfs and sum minus which is more concise", () => {
+        it("[], 3 --> false", () => {
+            const array = [];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfs(tree, 3);
+
+            expect(result).toEqual(false);
+        });
+
+        it("[1], 1 --> true", () => {
+            const array = [1];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfs(tree, 1);
+
+            expect(result).toEqual(true);
+        });
+
+        it("[1], 2 --> false", () => {
+            const array = [1];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfs(tree, 2);
+
+            expect(result).toEqual(false);
+        });
+
+        it("[1, null, null, 1, 2, 3, 4], 1 --> true", () => {
+            const array = [1, null, null, 1, 2, 3, 4];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfs(tree, 1);
+
+            expect(result).toEqual(true);
+        });
+
+        it("[1, null, null, 1, 2, 3, 4], 2 --> false", () => {
+            const array = [1, null, null, 1, 2, 3, 4];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfs(tree, 2);
+
+            expect(result).toEqual(false);
+        });
+
+        it("[3, null, 5, 4], 1 --> false", () => {
+            const array = [3, null, 5, 4];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfs(tree, 1);
+
+            expect(result).toEqual(false);
+        });
+
+        it("[3, null, 5, 4], 3 --> false", () => {
+            const array = [3, null, 5, 4];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfs(tree, 3);
+
+            expect(result).toEqual(false);
+        });
+
+        it("[6, null, 8, 7, 9], 23 --> true", () => {
+            const array = [6, null, 8, 7, 9];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfs(tree, 23);
+
+            expect(result).toEqual(true);
+        });
+
+        it("[6, null, 8, 7, 9], 21 --> true", () => {
+            const array = [6, null, 8, 7, 9];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfs(tree, 21);
+
+            expect(result).toEqual(true);
+        });
+
+        it("[6, null, 8, 7, 9], 33 --> false", () => {
+            const array = [6, null, 8, 7, 9];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfs(tree, 33);
+
+            expect(result).toEqual(false);
+        });
+
+        it("[6, 2, 9, null, 4, 7, 10, 3], 15 --> true", () => {
+            const array = [6, 2, 9, null, 4, 7, 10, 3];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfs(tree, 15);
+
+            expect(result).toEqual(true);
+        });
+
+
+        it("[6, 2, 9, null, 4, 7, 10, 3], 37 --> false", () => {
+            const array = [6, 2, 9, null, 4, 7, 10, 3];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfs(tree, 37);
+
+            expect(result).toEqual(false);
+        });
+
+        it("[6, 2, 9, null, 4, 7, 10, 3], 25 --> true", () => {
+            const array = [6, 2, 9, null, 4, 7, 10, 3];
+            const tree = arrayToBinaryTree(array);
+
+            const result = pathSum.dfs(tree, 25);
+
+            expect(result).toEqual(true);
+        });
+    });
+});
