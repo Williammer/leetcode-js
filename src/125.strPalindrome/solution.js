6c4804e634ab3d93e924b9dd3ba26a8b5af173cc
Williammer <supervegeter@gmail.com>
2017-01-13 08:54:50 +0800

    Added a solution that didn't use toLowerCase for 125.

--- src/125.strPalindrome/solution.js
+++ src/125.strPalindrome/solution.js
@@ -19,7 +19,7 @@
 export const strPalindrome = {};
 
 /**
- * Solution 1: two pointers shrink
+ * Solution 1: two pointers shrink, with tolowercase
  *
  * "N" is the string length.
  * Time complexity: O(N)
@@ -27,11 +27,6 @@ export const strPalindrome = {};
  */
 strPalindrome.twoPointers = (s) => {
     const isAlphaNumeric = (s) => {
-        const code = s.charCodeAt();
-
-        return (code >= 48 && code <= 57) ||
-            (code >= 97 && code <= 122);
-    }, insensitiveCompare = (a, b) => {
         const code = s.charCodeAt();
 
         return (code >= 48 && code <= 57) ||
@@ -49,13 +44,13 @@ strPalindrome.twoPointers = (s) => {
     s = s.toLowerCase();
 
     let pHead = 0,
-        pTail = s.length-1;
+        pTail = s.length - 1;
 
     while (pHead < pTail) {
-        while(!isAlphaNumeric(s[pHead]) && pHead < pTail) {
+        while (!isAlphaNumeric(s[pHead]) && pHead < pTail) {
             pHead++;
         }
-        while(!isAlphaNumeric(s[pTail]) && pHead < pTail) {
+        while (!isAlphaNumeric(s[pTail]) && pHead < pTail) {
             pTail--;
         }
 
@@ -70,10 +65,68 @@ strPalindrome.twoPointers = (s) => {
     return true;
 };
 
+/**
+ * Solution 2: two pointers shrink, no tolowercase
+ *
+ * "N" is the string length.
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ */
+strPalindrome.twoPointersInsens = (s) => {
+    const isAlphaNumeric = (s) => {
+            const code = s.charCodeAt();
+
+            return (code >= 48 && code <= 57) ||
+                (code >= 65 && code <= 90) ||
+                (code >= 97 && code <= 122);
+        },
+        insensitiveCompare = (a, b) => {
+            const codeA = a.charCodeAt(),
+                codeB = b.charCodeAt();
+
+            return (a === b ||
+                (codeA >= 65 && codeA <= 90 || codeA >= 97 && codeA <= 122) &&
+                (codeB >= 65 && codeB <= 90 || codeB >= 97 && codeB <= 122) &&
+                Math.abs(codeA - codeB) === 32);
+        };
+
+    if (typeof s !== "string") {
+        return false;
+    }
+
+    if (s.length === 0) {
+        return true;
+    }
+
+    let pHead = 0,
+        pTail = s.length - 1;
+
+    while (pHead < pTail) {
+        while (!isAlphaNumeric(s[pHead]) && pHead < pTail) {
+            pHead++;
+        }
+        while (!isAlphaNumeric(s[pTail]) && pHead < pTail) {
+            pTail--;
+        }
+
+        if (!insensitiveCompare(s[pHead], s[pTail])) {
+            return false;
+        }
+
+        pHead++;
+        pTail--;
+    }
+
+    return true;
+};
+
 
 /************************************************************************************************************************
 
  * Lessons:
-   1. Tackle palindrome with 2 pointers is effective.
+   1. Tackle palindrome with 2 pointers is effective and strightforward.
+   2. Be careful for the brackets added in the multiple boolean logic expressions,
+    '||' returns true if the before-part is true, so it needs proper brackets to restrict the scope.
+    eg. (ca >= 65 && ca <= 90) || (cb >= 97 && cb <= 122) will be different from (ca >= 65 && ca <= 90 || cb >= 97 && cb <= 122).
 
 ************************************************************************************************************************/
--- test/spec/125.strPalindrome.spec.js
+++ test/spec/125.strPalindrome.spec.js
@@ -2,7 +2,7 @@ import { strPalindrome } from "../../src/125.strPalindrome/solution";
 
 describe("# Problem 125 - whether the given string is palindrome. Only consider case-insensitive alphanumeric.", () => {
 
-    describe("Solution 1: two pointers shrink.", () => {
+    describe("Solution 1: two pointers shrink, with toLowerCase.", () => {
         it("'' -> true", () => {
             const result = strPalindrome.twoPointers("");
 
@@ -67,6 +67,86 @@ describe("# Problem 125 - whether the given string is palindrome. Only consider 
             const result = strPalindrome.twoPointers("A man, a plan, a canal: Panama");
 
             expect(result).toEqual(true);
+        });
+
+        it("',; W;:GlG:;l ;,' -> false", () => {
+            const result = strPalindrome.twoPointers(",; W;:GlG:;l ;,");
+
+            expect(result).toEqual(false);
+        });
+    });
+
+    describe("Solution 2: two pointers shrink, without toLowerCase.", () => {
+        it("'' -> true", () => {
+            const result = strPalindrome.twoPointersInsens("");
+
+            expect(result).toEqual(true);
+        });
+
+        it("';:\|{><?' -> true", () => {
+            const result = strPalindrome.twoPointersInsens(";:\|{><?");
+
+            expect(result).toEqual(true);
+        });
+
+        it("'a' -> true", () => {
+            const result = strPalindrome.twoPointersInsens("a");
+
+            expect(result).toEqual(true);
+        });
+
+        it("'aA' -> true", () => {
+            const result = strPalindrome.twoPointersInsens("aA");
+
+            expect(result).toEqual(true);
+        });
+
+        it("'.;:\|a.;:\|{><?a><?' -> true", () => {
+            const result = strPalindrome.twoPointersInsens(".;:\|a.;:\|{><?a><?");
+
+            expect(result).toEqual(true);
+        });
+
+        it("'ab' -> false", () => {
+            const result = strPalindrome.twoPointersInsens("ab");
+
+            expect(result).toEqual(false);
+        });
+
+        it("'race a car' -> false", () => {
+            const result = strPalindrome.twoPointersInsens("race a car");
+
+            expect(result).toEqual(false);
+        });
+
+        it("'raceacar' -> false", () => {
+            const result = strPalindrome.twoPointersInsens("raceacar");
+
+            expect(result).toEqual(false);
+        });
+
+        it("'abcdcba' -> true", () => {
+            const result = strPalindrome.twoPointersInsens("abcdcba");
+
+            expect(result).toEqual(true);
+        });
+
+        it("'abccdcba' -> false", () => {
+            const result = strPalindrome.twoPointersInsens("abccdcba");
+
+            expect(result).toEqual(false);
+        });
+
+        it("'A man, a plan, a canal: Panama' -> true", () => {
+            const result = strPalindrome.twoPointersInsens("A man, a plan, a canal: Panama");
+
+            expect(result).toEqual(true);
+        });
+
+        it("',; W;:GlG:;l ;,' -> false", () => {
+            const result = strPalindrome.twoPointersInsens(",; W;:GlG:;l ;,");
+
+            expect(result).toEqual(false);
         });
     });
 
