3ea60b5717438e1df98d685a6b055ad56d10c7ea
William He <supervegeter@gmail.com>
2017-02-01 12:49:45 +0800

    Abandoned babel-node; use cjs and rollup for node test instead.

--- test/testNode.js
+++ test/testNode.js
@@ -1,42 +1,67 @@
-import fs from "fs-extra";
-import Jasmine from "jasmine";
-import { SpecReporter } from "jasmine-spec-reporter";
+const fs = require("fs-extra"),
+  Jasmine = require("jasmine"),
+  SpecReporter = require("jasmine-spec-reporter").SpecReporter,
+  rollup = require("rollup"),
+  multiEntry = require("rollup-plugin-multi-entry"),
+  istanbul = require("rollup-plugin-istanbul");
 
 const noop = function() {},
-    num = process.argv[2];
+  num = process.argv[2],
+  destJs = `test/spec/testSpecs.js`;
 
 let jrunner = new Jasmine();
 
 jasmine.getEnv().clearReporters();
 jrunner.configureDefaultReporter({ print: noop });
 jasmine.getEnv().addReporter(new SpecReporter({ // add jasmine-spec-reporter
-    spec: {
-        displayPending: true
-    }
+  spec: {
+    displayPending: true
+  }
 }));
 
 if (num) {
-    // single problem test
-    fs.readdir(`./test/spec/`, (err, files) => {
-        if (err) {
-            console.warn(`[readdir] err: ${err}`);
-            return;
-        }
+  // single problem test
+  fs.readdir(`./test/spec/`, (err, files) => {
+    if (err) {
+      console.warn(`[readdir] err: ${err}`);
+      return;
+    }
 
-        let targetFilename = files.filter(file => {
-            return file.indexOf(`${num}.`) === 0;
-        });
-
-        if (!targetFilename || !(targetFilename.length > 0)) {
-            console.warn(`[readdir] can't find target file to test.`);
-            return;
-        } else {
-            jrunner.execute([`test/spec/${targetFilename}`]);
-        }
+    let targetFilename = files.filter(file => {
+      return file.indexOf(`${num}.`) === 0;
     });
 
+    if (!targetFilename || !(targetFilename.length > 0)) {
+      console.warn(`[readdir] can't find target file to test.`);
+      return;
+    } else {
+      const specPath = `test/spec/${targetFilename}`;
+      rollup.rollup({
+        entry: specPath
+      }).then(function(bundle) {
+        bundle.write({
+          format: `es`,
+          dest: destJs
+        }).then(function() {
+          jrunner.execute([destJs]);
+        });
+      });
+    }
+  });
+
 } else {
-    // all problem test
-    jrunner.loadConfigFile(`test/jasmine.config.json`);
-    jrunner.execute();
+  // all problem test
+  rollup.rollup({
+    entry: `test/spec/*.spec.js`,
+    plugins: [
+      multiEntry()
+    ]
+  }).then(function(bundle) {
+    bundle.write({
+      format: `es`,
+      dest: destJs
+    }).then(function() {
+      jrunner.execute([destJs]);
+    });
+  });
 }
