71cbf981bba5f3b752235cf4c37e61ecfea4b3b6
William He <supervegeter@gmail.com>
2017-02-11 13:53:21 +0800

    Changed to indent 2.

--- js/leetcode-table.js
+++ js/leetcode-table.js
@@ -2,193 +2,195 @@
  * leetcode table with Vue.js
  ***/
 (function($) {
-    /**
-     * v-table component
-     */
-    const VTableComponent = {
-        template: '<table>\
-            <thead>\
-                <tr>\
-                    <th v-for="key in columns" @click="sortBy(key)" :class="{ active: sortKey == key }">\
-                        {{ key | capitalize }}\
-                        <span class="arrow" :class="sortOrders[key] > 0 ? \'dsc\' : \'asc\'"></span>\
-                    </th>\
-                </tr>\
-            </thead>\
-                <tbody name="leetcode-tb-tr"\
-                    is="transition-group"\
-                    v-bind:css="false"\
-                    v-on:before-enter="beforeEnter"\
-                    v-on:enter="enter"\
-                    v-on:leave="leave"\
-                >\
-                    <tr v-for="(entry, index) in filteredData"\
-                        :key="entry.num"\
-                        :data-index="index"\
-                    >\
-                        <template v-for="key in columns">\
-                            <td v-if="isLinkEntry(key)">\
-                                <v-link :link="entry[key]"></v-link>\
-                            </td>\
-                            <td v-else>\
-                                {{entry[key]}}\
-                            </td>\
-                        </template>\
-                    </tr>\
-                </transition-group>\
-        </table>',
+  /**
+   * v-table component
+   */
+  const VTableComponent = {
+    template: '\
+      <table>\
+        <thead>\
+          <tr>\
+            <th v-for="key in columns" @click="sortBy(key)" :class="{ active: sortKey == key }">\
+              {{ key | capitalize }}\
+              <span class="arrow" :class="sortOrders[key] > 0 ? \'dsc\' : \'asc\'"></span>\
+            </th>\
+          </tr>\
+        </thead>\
+        <tbody name="leetcode-tb-tr"\
+          is="transition-group"\
+          v-bind:css="false"\
+          v-on:before-enter="beforeEnter"\
+          v-on:enter="enter"\
+          v-on:leave="leave"\
+        >\
+          <tr v-for="(entry, index) in filteredData"\
+            :key="entry.num"\
+            :data-index="index"\
+          >\
+            <template v-for="key in columns">\
+              <td v-if="isLinkEntry(key)">\
+                <v-link :link="entry[key]"></v-link>\
+              </td>\
+              <td v-else>\
+                {{entry[key]}}\
+              </td>\
+            </template>\
+          </tr>\
+        </transition-group>\
+      </table>\
+    ',
 
-        components: {
-            'v-link': {
-                props: { link: { type: String, required: true } },
-                template: '<a @click="showSourceCode(link)" id="v-link">solution</a>',
-                methods: {
-                    setModalContent: function(data) {
-                        let vModal = document.getElementById('v-modal'),
-                            preElement = document.createElement('pre'),
-                            codeElement = document.createElement('code');
-
-                        // clear #v-modal element
-                        vModal.innerHTML = "";
-
-                        // append sourceCode
-                        codeElement.innerHTML = data;
-                        codeElement.className = 'language-javascript';
-                        preElement.appendChild(codeElement);
-                        vModal.appendChild(preElement);
-
-                        $.Prism.highlightAll();
-
-                        return vModal;
-                    },
-                    showModal: function(data) {
-                        const vModalContent = this.setModalContent(data);
-
-                        new $.Modal({
-                            content: vModalContent,
-                            className: 'zoom'
-                        }).open();
-
-                        // TODO: clear up upon close modal.
-                    },
-                    showSourceCode: function(url) {
-                        if (!(typeof url === 'string' && url.length > 0)) {
-                            console.warn('[showSourceCode] invalid url.');
-                            return;
-                        }
-
-                        $.fetch(url)
-                            .then(function(response) {
-                                return response.text();
-                            })
-                            .then(function(data) {
-                                this.showModal(data);
-                            }.bind(this))
-                            .catch(function(ex) {
-                                console.warn('get solution.js failed', ex);
-                            });
-                    }
-                }
-            }
-        },
-        replace: true,
-        props: {
-            data: Array,
-            columns: Array,
-            filterKey: String
-        },
-        /* derived data state model */
-        data: function() {
-            let sortOrders = {};
-            this.columns.forEach(function(key) {
-                sortOrders[key] = 1
-            });
-
-            return {
-                sortKey: 'num',
-                sortOrders: sortOrders
-            };
-        },
-        computed: {
-            filteredData: function() {
-                let sortKey = this.sortKey,
-                    filterKey = this.filterKey && this.filterKey.toLowerCase(),
-                    order = this.sortOrders[sortKey] || 1,
-                    data = this.data;
-
-                if (filterKey) {
-                    data = data.filter(function(row) {
-                        return Object.keys(row).some(function(key) {
-                            return String(row[key]).toLowerCase().indexOf(filterKey) > -1;
-                        });
-                    });
-                }
-                if (sortKey) {
-                    data = data.slice().sort(function(a, b) {
-                        a = a[sortKey];
-                        b = b[sortKey];
-                        return (a === b ? 0 : a > b ? 1 : -1) * order;
-                    });
-                }
-
-                return data;
-            }
-        },
-        filters: {
-            capitalize: function(str) {
-                return str.charAt(0).toUpperCase() + str.slice(1);
-            }
-        },
+    components: {
+      'v-link': {
+        props: { link: { type: String, required: true } },
+        template: '<a @click="showSourceCode(link)" id="v-link">solution</a>',
         methods: {
-            isLinkEntry: function(key) {
-                return key === 'solution.js';
-            },
-            sortBy: function(key) {
-                this.sortKey = key;
-                if (!this.sortOrders[key]) {
-                    this.sortOrders[key] = 1;
-                }
-                this.sortOrders[key] = this.sortOrders[key] * -1;
-            },
-            beforeEnter: function(el) {
-                el.style.opacity = 0;
-            },
-            enter: function(el, done) {
-                $.Velocity(
-                    el, { opacity: 1 }, { complete: done }
-                );
-            },
-            leave: function(el, done) {
-                $.Velocity(
-                    el, { opacity: 0 }, { complete: done }
-                );
-            }
-        }
-    };
+          setModalContent: function(data) {
+            let vModal = document.getElementById('v-modal'),
+              preElement = document.createElement('pre'),
+              codeElement = document.createElement('code');
 
-    /**
-     * Init - get leetcode data from remote then display in v-table.
-     */
-    $.fetch('https://raw.githubusercontent.com/Williammer/leetcode-js/master/leetcode-data.json')
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(tbData) {
-            // console.debug('parsed json', tbData)
-            new Vue({
-                el: '#leetcode-table',
-                components: {
-                    'v-table': VTableComponent
-                },
-                data: {
-                    searchQuery: '',
-                    tbColumns: ['num', 'title', 'tags', 'level', 'solution.js'],
-                    tbData: tbData
-                }
+            // clear #v-modal element
+            vModal.innerHTML = "";
+
+            // append sourceCode
+            codeElement.innerHTML = data;
+            codeElement.className = 'language-javascript';
+            preElement.appendChild(codeElement);
+            vModal.appendChild(preElement);
+
+            $.Prism.highlightAll();
+
+            return vModal;
+          },
+          showModal: function(data) {
+            const vModalContent = this.setModalContent(data);
+
+            new $.Modal({
+              content: vModalContent,
+              className: 'zoom'
+            }).open();
+
+            // TODO: clear up upon close modal.
+          },
+          showSourceCode: function(url) {
+            if (!(typeof url === 'string' && url.length > 0)) {
+              console.warn('[showSourceCode] invalid url.');
+              return;
+            }
+
+            $.fetch(url)
+              .then(function(response) {
+                return response.text();
+              })
+              .then(function(data) {
+                this.showModal(data);
+              }.bind(this))
+              .catch(function(ex) {
+                console.warn('get solution.js failed', ex);
+              });
+          }
+        }
+      }
+    },
+    replace: true,
+    props: {
+      data: Array,
+      columns: Array,
+      filterKey: String
+    },
+    /* derived data state model */
+    data: function() {
+      let sortOrders = {};
+      this.columns.forEach(function(key) {
+        sortOrders[key] = 1
+      });
+
+      return {
+        sortKey: 'num',
+        sortOrders: sortOrders
+      };
+    },
+    computed: {
+      filteredData: function() {
+        let sortKey = this.sortKey,
+          filterKey = this.filterKey && this.filterKey.toLowerCase(),
+          order = this.sortOrders[sortKey] || 1,
+          data = this.data;
+
+        if (filterKey) {
+          data = data.filter(function(row) {
+            return Object.keys(row).some(function(key) {
+              return String(row[key]).toLowerCase().indexOf(filterKey) > -1;
             });
-        })
-        .catch(function(ex) {
-            console.warn('parsing failed', ex);
-        });
+          });
+        }
+        if (sortKey) {
+          data = data.slice().sort(function(a, b) {
+            a = a[sortKey];
+            b = b[sortKey];
+            return (a === b ? 0 : a > b ? 1 : -1) * order;
+          });
+        }
+
+        return data;
+      }
+    },
+    filters: {
+      capitalize: function(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+      }
+    },
+    methods: {
+      isLinkEntry: function(key) {
+        return key === 'solution.js';
+      },
+      sortBy: function(key) {
+        this.sortKey = key;
+        if (!this.sortOrders[key]) {
+          this.sortOrders[key] = 1;
+        }
+        this.sortOrders[key] = this.sortOrders[key] * -1;
+      },
+      beforeEnter: function(el) {
+        el.style.opacity = 0;
+      },
+      enter: function(el, done) {
+        $.Velocity(
+          el, { opacity: 1 }, { complete: done }
+        );
+      },
+      leave: function(el, done) {
+        $.Velocity(
+          el, { opacity: 0 }, { complete: done }
+        );
+      }
+    }
+  };
+
+  /**
+   * Init - get leetcode data from remote then display in v-table.
+   */
+  $.fetch('https://raw.githubusercontent.com/Williammer/leetcode-js/master/leetcode-data.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(tbData) {
+      // console.debug('parsed json', tbData)
+      new Vue({
+        el: '#leetcode-table',
+        components: {
+          'v-table': VTableComponent
+        },
+        data: {
+          searchQuery: '',
+          tbColumns: ['num', 'title', 'tags', 'level', 'solution.js'],
+          tbData: tbData
+        }
+      });
+    })
+    .catch(function(ex) {
+      console.warn('parsing failed', ex);
+    });
 
 })(window);
--- js/modal.js
+++ js/modal.js
@@ -1,145 +1,145 @@
 // Create an immediately invoked functional expression to wrap our code
 (function() {
-    // Define our constructor
-    this.Modal = function() {
-        // Create global element references
-        this.closeButton = null;
-        this.modal = null;
-        this.overlay = null;
+  // Define our constructor
+  this.Modal = function() {
+    // Create global element references
+    this.closeButton = null;
+    this.modal = null;
+    this.overlay = null;
 
-        // Determine proper prefix
-        this.transitionEnd = transitionSelect();
+    // Determine proper prefix
+    this.transitionEnd = transitionSelect();
 
-        // Define option defaults
-        var defaults = {
-            autoOpen: false,
-            className: 'fade-and-drop',
-            closeButton: true,
-            content: "",
-            // maxWidth: 600,
-            minWidth: 280,
-            overlay: true
-        }
-
-        // Create options by extending defaults with the passed in arugments
-        if (arguments[0] && typeof arguments[0] === "object") {
-            this.options = extendDefaults(defaults, arguments[0]);
-        }
-
-        if (this.options.autoOpen === true) this.open();
+    // Define option defaults
+    var defaults = {
+      autoOpen: false,
+      className: 'fade-and-drop',
+      closeButton: true,
+      content: "",
+      // maxWidth: 600,
+      minWidth: 280,
+      overlay: true
     }
 
-    // Public Methods
-
-    Modal.prototype.close = function() {
-        var _ = this;
-        this.modal.className = this.modal.className.replace(" scotch-open", "");
-        this.modal.addEventListener(this.transitionEnd, function() {
-            _.modal.parentNode.removeChild(_.modal);
-        });
-
-        if (this.overlay) {
-            this.overlay.className = this.overlay.className.replace(" scotch-open", "");
-            this.overlay.addEventListener(this.transitionEnd, function() {
-                if (_.overlay.parentNode) _.overlay.parentNode.removeChild(_.overlay);
-            });
-        }
-
-        document.body.className = "";
+    // Create options by extending defaults with the passed in arugments
+    if (arguments[0] && typeof arguments[0] === "object") {
+      this.options = extendDefaults(defaults, arguments[0]);
     }
 
-    Modal.prototype.open = function() {
-        buildOut.call(this);
-        initializeEvents.call(this);
-        window.getComputedStyle(this.modal).height;
-        this.modal.className = this.modal.className +
-            (this.modal.offsetHeight > window.innerHeight ?
-                " scotch-open scotch-anchored" : " scotch-open");
+    if (this.options.autoOpen === true) this.open();
+  }
 
-        if (this.overlay) {
-            this.overlay.className = this.overlay.className + " scotch-open";
-        }
+  // Public Methods
 
-        window.scrollTo(0, document.body.scrollHeight / 4.8);
-        document.body.className = "noscroll";
+  Modal.prototype.close = function() {
+    var _ = this;
+    this.modal.className = this.modal.className.replace(" scotch-open", "");
+    this.modal.addEventListener(this.transitionEnd, function() {
+      _.modal.parentNode.removeChild(_.modal);
+    });
+
+    if (this.overlay) {
+      this.overlay.className = this.overlay.className.replace(" scotch-open", "");
+      this.overlay.addEventListener(this.transitionEnd, function() {
+        if (_.overlay.parentNode) _.overlay.parentNode.removeChild(_.overlay);
+      });
     }
 
-    // Private Methods
-    function buildOut() {
-        var content, contentHolder, docFrag;
+    document.body.className = "";
+  }
 
-        /*
-         * If content is an HTML string, append the HTML string.
-         * If content is a domNode, append its content.
-         */
+  Modal.prototype.open = function() {
+    buildOut.call(this);
+    initializeEvents.call(this);
+    window.getComputedStyle(this.modal).height;
+    this.modal.className = this.modal.className +
+      (this.modal.offsetHeight > window.innerHeight ?
+        " scotch-open scotch-anchored" : " scotch-open");
 
-        if (typeof this.options.content === "string") {
-            content = this.options.content;
-        } else {
-            content = this.options.content.innerHTML;
-        }
-
-        // Create a DocumentFragment to build with
-        docFrag = document.createDocumentFragment();
-
-        // Create modal element
-        this.modal = document.createElement("div");
-        this.modal.className = "scotch-modal " + this.options.className;
-        this.modal.style.minWidth = this.options.minWidth + "px";
-        this.modal.style.maxWidth = this.options.maxWidth + "px";
-
-        // If closeButton option is true, add a close button
-        if (this.options.closeButton === true) {
-            this.closeButton = document.createElement("button");
-            this.closeButton.className = "scotch-close close-button";
-            this.closeButton.innerHTML = "&times;";
-            this.modal.appendChild(this.closeButton);
-        }
-
-        // If overlay is true, add one
-        if (this.options.overlay === true) {
-            this.overlay = document.createElement("div");
-            this.overlay.className = "scotch-overlay " + this.options.className;
-            docFrag.appendChild(this.overlay);
-        }
-
-        // Create content area and append to modal
-        contentHolder = document.createElement("div");
-        contentHolder.className = "scotch-content";
-        contentHolder.innerHTML = content;
-        this.modal.appendChild(contentHolder);
-
-        // Append modal to DocumentFragment
-        docFrag.appendChild(this.modal);
-
-        // Append DocumentFragment to body
-        document.body.appendChild(docFrag);
+    if (this.overlay) {
+      this.overlay.className = this.overlay.className + " scotch-open";
     }
 
-    function extendDefaults(source, properties) {
-        var property;
-        for (property in properties) {
-            if (properties.hasOwnProperty(property)) {
-                source[property] = properties[property];
-            }
-        }
-        return source;
+    window.scrollTo(0, document.body.scrollHeight / 4.8);
+    document.body.className = "noscroll";
+  }
+
+  // Private Methods
+  function buildOut() {
+    var content, contentHolder, docFrag;
+
+    /*
+     * If content is an HTML string, append the HTML string.
+     * If content is a domNode, append its content.
+     */
+
+    if (typeof this.options.content === "string") {
+      content = this.options.content;
+    } else {
+      content = this.options.content.innerHTML;
     }
 
-    function initializeEvents() {
-        if (this.closeButton) {
-            this.closeButton.addEventListener('click', this.close.bind(this));
-        }
+    // Create a DocumentFragment to build with
+    docFrag = document.createDocumentFragment();
 
-        if (this.overlay) {
-            this.overlay.addEventListener('click', this.close.bind(this));
-        }
+    // Create modal element
+    this.modal = document.createElement("div");
+    this.modal.className = "scotch-modal " + this.options.className;
+    this.modal.style.minWidth = this.options.minWidth + "px";
+    this.modal.style.maxWidth = this.options.maxWidth + "px";
+
+    // If closeButton option is true, add a close button
+    if (this.options.closeButton === true) {
+      this.closeButton = document.createElement("button");
+      this.closeButton.className = "scotch-close close-button";
+      this.closeButton.innerHTML = "&times;";
+      this.modal.appendChild(this.closeButton);
     }
 
-    function transitionSelect() {
-        var el = document.createElement("div");
-        if (el.style.WebkitTransition) return "webkitTransitionEnd";
-        if (el.style.OTransition) return "oTransitionEnd";
-        return 'transitionend';
+    // If overlay is true, add one
+    if (this.options.overlay === true) {
+      this.overlay = document.createElement("div");
+      this.overlay.className = "scotch-overlay " + this.options.className;
+      docFrag.appendChild(this.overlay);
     }
+
+    // Create content area and append to modal
+    contentHolder = document.createElement("div");
+    contentHolder.className = "scotch-content";
+    contentHolder.innerHTML = content;
+    this.modal.appendChild(contentHolder);
+
+    // Append modal to DocumentFragment
+    docFrag.appendChild(this.modal);
+
+    // Append DocumentFragment to body
+    document.body.appendChild(docFrag);
+  }
+
+  function extendDefaults(source, properties) {
+    var property;
+    for (property in properties) {
+      if (properties.hasOwnProperty(property)) {
+        source[property] = properties[property];
+      }
+    }
+    return source;
+  }
+
+  function initializeEvents() {
+    if (this.closeButton) {
+      this.closeButton.addEventListener('click', this.close.bind(this));
+    }
+
+    if (this.overlay) {
+      this.overlay.addEventListener('click', this.close.bind(this));
+    }
+  }
+
+  function transitionSelect() {
+    var el = document.createElement("div");
+    if (el.style.WebkitTransition) return "webkitTransitionEnd";
+    if (el.style.OTransition) return "oTransitionEnd";
+    return 'transitionend';
+  }
 }());
