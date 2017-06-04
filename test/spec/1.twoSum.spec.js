4bb41901311e073e4a831c05da0024d020654e15
Williammer <supervegeter@gmail.com>
2017-06-04 21:16:56 +0800

    Refactored unit test of 1.twoSum, which reduced duplications.

--- test/spec/1.twoSum.spec.js
+++ test/spec/1.twoSum.spec.js
@@ -1,147 +1,91 @@
-import { twoSum } from "../../src/1.twoSum/solution"
+import { twoSum } from "../../src/1.twoSum/solution";
+import { runSuites } from "../helper.js";
+
 
 describe("# Problem 1 - Two sum (assume that each input would have exactly one solution)", () => {
+  const solutions = ["brutal", "hash"];
 
-    // # Solution 1: brutal loop
-    describe("Solution 1: brutal loop", () => {
-        it("return expected result for even nums array:  [2, 7, 11, 15] & 9 => [0, 1]", () => {
-            const nums = [2, 7, 11, 15];
-            const target = 9;
+  const allTestSuites = [{
+    title: "return expected result for even nums array:  [2, 7, 11, 15] & 9 => [0, 1]",
+    suite: (solutionName) => () => {
+      const nums = [2, 7, 11, 15];
+      const target = 9;
 
-            const result = twoSum.brutal(nums, target);
-            expect(result).toEqual([0, 1]);
-        });
+      const result = twoSum[solutionName](nums, target);
+      expect(result).toEqual([0, 1]);
+    },
+  }, {
 
-        it("return expected result for odd nums array:  [2, 20, 11, 7, 15] & 9 => [0, 3]", () => {
-            const nums = [2, 20, 11, 7, 15];
-            const target = 9;
+    title: "return expected result for odd nums array:  [2, 20, 11, 7, 15] & 9 => [0, 3]",
+    suite: (solutionName) => () => {
+      const nums = [2, 20, 11, 7, 15];
+      const target = 9;
 
-            const result = twoSum.brutal(nums, target);
-            expect(result).toEqual([0, 3]);
-        });
+      const result = twoSum[solutionName](nums, target);
+      expect(result).toEqual([0, 3]);
+    },
+  }, {
 
-        it("return undefined if no desired result:  [2, 7, 11, 15] & 8 => undefined", () => {
-            const nums = [2, 7, 11, 15];
-            const target = 8;
+    title: "return undefined if no desired result:  [2, 7, 11, 15] & 8 => undefined",
+    suite: (solutionName) => () => {
+      const nums = [2, 7, 11, 15];
+      const target = 8;
 
-            const result = twoSum.brutal(nums, target);
-            expect(result).toBeUndefined();
-        });
+      const result = twoSum[solutionName](nums, target);
+      expect(result).toBeUndefined();
+    },
+  }, {
 
-        it("return undefined if nums is not array", () => {
-            const foo = 42,
-                bar = "baz",
-                wow = undefined,
-                much = {},
-                fn = () => {},
-                test = null;
+    title: "return undefined if nums is not array",
+    suite: (solutionName) => () => {
+      const foo = 42,
+        bar = "baz",
+        wow = undefined,
+        much = {},
+        fn = () => {},
+        test = null;
 
-            const target = 9;
+      const target = 9;
 
-            expect(twoSum.brutal(foo, target)).toBeUndefined();
-            expect(twoSum.brutal(bar, target)).toBeUndefined();
-            expect(twoSum.brutal(wow, target)).toBeUndefined();
-            expect(twoSum.brutal(much, target)).toBeUndefined();
-            expect(twoSum.brutal(fn, target)).toBeUndefined();
-            expect(twoSum.brutal(test, target)).toBeUndefined();
-        });
+      expect(twoSum[solutionName](foo, target)).toBeUndefined();
+      expect(twoSum[solutionName](bar, target)).toBeUndefined();
+      expect(twoSum[solutionName](wow, target)).toBeUndefined();
+      expect(twoSum[solutionName](much, target)).toBeUndefined();
+      expect(twoSum[solutionName](fn, target)).toBeUndefined();
+      expect(twoSum[solutionName](test, target)).toBeUndefined();
+    },
+  }, {
 
-        it("return undefined if nums is an array with less than 2 values:  [2] & 9 => undefined", () => {
-            const nums = [2];
-            const target = 9;
+    title: "return undefined if nums is an array with less than 2 values:  [2] & 9 => undefined",
+    suite: (solutionName) => () => {
+      const nums = [2];
+      const target = 9;
 
-            const result = twoSum.brutal(nums, target);
-            expect(result).toBeUndefined();
-        });
+      const result = twoSum[solutionName](nums, target);
+      expect(result).toBeUndefined();
+    },
+  }, {
 
-        it("return undefined if target is not a number", () => {
-            const foo = [],
-                bar = "baz",
-                wow = undefined,
-                much = {},
-                fn = () => {},
-                test = null;
+    title: "return undefined if target is not a number",
+    suite: (solutionName) => () => {
+      const foo = [],
+        bar = "baz",
+        wow = undefined,
+        much = {},
+        fn = () => {},
+        test = null;
 
-            const nums = [2, 7, 11, 15];
+      const nums = [2, 7, 11, 15];
 
-            expect(twoSum.brutal(nums, foo)).toBeUndefined();
-            expect(twoSum.brutal(nums, bar)).toBeUndefined();
-            expect(twoSum.brutal(nums, wow)).toBeUndefined();
-            expect(twoSum.brutal(nums, much)).toBeUndefined();
-            expect(twoSum.brutal(nums, fn)).toBeUndefined();
-            expect(twoSum.brutal(nums, test)).toBeUndefined();
-        });
-    });
+      expect(twoSum[solutionName](nums, foo)).toBeUndefined();
+      expect(twoSum[solutionName](nums, bar)).toBeUndefined();
+      expect(twoSum[solutionName](nums, wow)).toBeUndefined();
+      expect(twoSum[solutionName](nums, much)).toBeUndefined();
+      expect(twoSum[solutionName](nums, fn)).toBeUndefined();
+      expect(twoSum[solutionName](nums, test)).toBeUndefined();
+    },
+  }];
 
 
-    // # Solution 2: hash
-    describe("Solution 2: hash", () => {
-        it("return expected result for even nums array:  [2, 7, 11, 15] & 9 => [0, 1]", () => {
-            const nums = [2, 7, 11, 15];
-            const target = 9;
-
-            const result = twoSum.hash(nums, target);
-            expect(result).toEqual([0, 1]);
-        });
-
-        it("return expected result for odd nums array:  [2, 20, 11, 7, 15] & 9 => [0, 3]", () => {
-            const nums = [2, 20, 11, 7, 15];
-            const target = 9;
-
-            const result = twoSum.hash(nums, target);
-            expect(result).toEqual([0, 3]);
-        });
-
-        it("return undefined if no desired result:  [2, 7, 11, 15] & 8 => undefined", () => {
-            const nums = [2, 7, 11, 15];
-            const target = 8;
-
-            const result = twoSum.hash(nums, target);
-            expect(result).toBeUndefined();
-        });
-
-        it("return undefined if nums is not array", () => {
-            const foo = 42,
-                bar = "baz",
-                wow = undefined,
-                much = {},
-                fn = () => {},
-                test = null;
-
-            const target = 9;
-
-            expect(twoSum.hash(foo, target)).toBeUndefined();
-            expect(twoSum.hash(bar, target)).toBeUndefined();
-            expect(twoSum.hash(wow, target)).toBeUndefined();
-            expect(twoSum.hash(much, target)).toBeUndefined();
-            expect(twoSum.hash(fn, target)).toBeUndefined();
-            expect(twoSum.hash(test, target)).toBeUndefined();
-        });
-
-        it("return undefined if nums is an array with less than 2 values:  [2] & 9 => undefined", () => {
-            const nums = [2];
-            const target = 9;
-
-            const result = twoSum.hash(nums, target);
-            expect(result).toBeUndefined();
-        });
-
-        it("return undefined if target is not a number", () => {
-            const foo = [],
-                bar = "baz",
-                wow = undefined,
-                much = {},
-                fn = () => {},
-                test = null;
-
-            const nums = [2, 7, 11, 15];
-
-            expect(twoSum.hash(nums, foo)).toBeUndefined();
-            expect(twoSum.hash(nums, bar)).toBeUndefined();
-            expect(twoSum.hash(nums, wow)).toBeUndefined();
-            expect(twoSum.hash(nums, much)).toBeUndefined();
-            expect(twoSum.hash(nums, fn)).toBeUndefined();
-            expect(twoSum.hash(nums, test)).toBeUndefined();
-        });
-    });
+  runSuites(allTestSuites, solutions);
 });
