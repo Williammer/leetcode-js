246b30e946aadb8b6963d10ad2ca35fb4968a572
Williammer <supervegeter@gmail.com>
2018-06-06 22:28:20 +0800

    minor

--- src/632.shortestRangeInKSortedList/solution.js
+++ src/632.shortestRangeInKSortedList/solution.js
@@ -1,6 +1,8 @@
 /************************************************************************************************************************
 
- * Problem: You have k lists of sorted integers in ascending order. Find the smallest range that includes at least one number from each of the k lists.
+ * Problem: https://leetcode.com/problems/smallest-range/description/
+    You have k lists of sorted integers in ascending order.
+    Find the smallest range that includes at least one number from each of the k lists.
     We define the range [a,b] is smaller than range [c,d] if b-a < d-c or a < c if b-a == d-c.
 
  * Example 1:
