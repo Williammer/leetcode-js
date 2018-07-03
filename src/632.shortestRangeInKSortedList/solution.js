4b25a47594a62efaecb0301b72db3d8bf871e7f8
Williammer <supervegeter@gmail.com>
2018-07-03 11:48:12 +0800

    Solved 632 with basic solution

--- src/632.shortestRangeInKSortedList/solution.js
+++ src/632.shortestRangeInKSortedList/solution.js
@@ -9,34 +9,85 @@
     Input:[[4,10,15,24,26], [0,9,12,20], [5,18,22,30]]
     Output: [20,24]
 
-
  * @param {number[][]} nums
  * @return {number[]}
 
- * Analysis:
- *  Interfence 1: The start and end num of the range has to be within two different lists.
- *  
-
 ************************************************************************************************************************/
 
-
 /**
- * Solution 1: 
+ * Solution 1: Bump up the list pointer of the smallest num to seek smaller range.
+ * Start from smallest num of each list, find the minimized num and move its pointer to next num,
+ *  in an attempt to seek for smaller range.
  *
- * "N" is 
- * Time complexity: O()
- * Space complexity: O()
+ * (N: The num of times to get the num range of each number combination of lists.)
+ * Time complexity: [O(1), O(K * N/2), O(K * N)]
+ * Space complexity: O(K)
  */
 export function normal(nums) {
+  if (!Array.isArray(nums) || nums.length <= 0) {
+    throw new Error('Invalid lists input provided');
+  }
 
+  const pointers = nums.map(() => 0);
+  let minIndex = 0;
+  let result = [null, null];
+  if (nums.length === 1) {
+    return [nums[0][0], nums[0][0]];
+  }
 
-  return;
-};
+  while (pointers[minIndex] < nums[minIndex].length) {
+    let minValue = null;
+    let maxValue = null;
+    let maxIndex;
+    const setMax = (value, index) => {
+      maxValue = value;
+      maxIndex = index;
+    };
+    const setMin = (value, index) => {
+      minValue = value;
+      minIndex = index;
+    };
 
+    pointers.forEach((pointer, index) => {
+      const value = nums[index][pointer];
+
+      if (minValue === null) {
+        setMin(value, index);
+      } else if (maxValue === null) {
+        if (value < minValue) {
+          setMax(minValue, minIndex);
+          setMin(value, index);
+        } else {
+          setMax(value, index);
+        }
+      } else if (value < minValue) {
+        setMin(value, index);
+      } else if (value > maxValue) {
+        setMax(value, index);
+      }
+    });
+
+    if (result[0] === null ||
+        result[1] === null ||
+        maxValue - minValue < result[1] - result[0]
+    ) {
+      result = [minValue, maxValue];
+    }
+
+    if (maxValue === minValue) {
+      break;
+    }
+
+    pointers[minIndex]++;
+  }
+
+  return result;
+}
 
 /************************************************************************************************************************
 
  * Lessons:
-   1. 
+   1. Analyse what is needed to do to get closer to find the smallest range, which is to
+      bump up the index of the min edge of the current smallest range.
 
 ************************************************************************************************************************/
--- test/spec/632.shortestRangeInKSortedList.spec.js
+++ test/spec/632.shortestRangeInKSortedList.spec.js
@@ -1,36 +1,56 @@
-import {
-  normal,
-} from "../../src/632.shortestRangeInKSortedList/solution";
+import { normal } from "../../src/632.shortestRangeInKSortedList/solution";
 
 describe("# Problem 632 - Shortest range for K sorted lists.", () => {
-  const inputs = [{
-    desc: 'regular, not same length',
-    data: [[4, 10, 15, 24, 26], [0, 9, 12, 20], [5, 18, 22, 30]],
-    result: [20, 24],
-  }];
+  const dataSets = [
+    {
+      data: [[1]],
+      result: [1, 1],
+    },
+    {
+      data: [[11, 11], [10, 10]],
+      result: [10, 11],
+    },
+    {
+      data: [[0, 9, 11, 20], [3, 10, 12, 13]],
+      result: [9, 10],
+    },
+    {
+      data: [[11], [11], [11], [11], [11], [11]],
+      result: [11, 11],
+    },
+    {
+      data: [[1, 5, 7, 14], [0, 9, 11, 20], [3, 10, 12, 13]],
+      result: [0, 3],
+    },
+    {
+      data: [[4, 8, 12, 14], [0, 9, 11, 20], [5, 10, 12, 30]],
+      result: [11, 12],
+    },
+    {
+      data: [[4, 10, 15, 24, 26], [0, 9, 12, 20], [5, 18, 22, 30]],
+      result: [20, 24],
+    },
+    {
+      data: [[4, 10, 15, 24, 26], [0, 9, 11, 20], [5, 14, 18, 22, 30]],
+      result: [10, 14],
+    },
+  ];
 
-  inputs.forEach(({ desc, data: lists, result }) => {
-    describe(`Solution 1:  (input : ${desc})`, () => {
-      it("the range should contain at least one item from each list", () => {
-        const [ start, end ] = normal(lists);
-        const allListInRange = lists.every((list) => !!list.find((item) => (item >= start && item <= end)));
-        expect(allListInRange).toBe(true);
-      });
+    describe("Solution 1: move the pointer of smallest num", () => {
+      dataSets.forEach(({ data: lists, result }) => {
+        describe(`input: ${JSON.stringify(lists)} => output: ${JSON.stringify(result)}`, () => {
+          it("the range should contain at least one item from each list", () => {
+            const [start, end] = normal(lists);
+            const allListInRange = lists.every(
+              list => list.find(item => item >= start && item <= end) !== undefined
+            );
+            expect(allListInRange).toBe(true);
+          });
 
-      it("should find the shortest range of items from K sorted lists", () => {
-        expect(normal(lists)).toEqual(result);
+          it("should find the shortest range of items from K sorted lists", () => {
+            expect(normal(lists)).toEqual(result);
+          });
+        });
       });
-    });
   });
-
-  describe('Extra cases', () => {
-    describe("Solution 1", () => {
-      it("prefer the range with smaller start number if two or more ranges are same", () => {
-        const input = [[4, 10, 15, 24, 26], [0, 9, 11, 20], [5, 14, 18, 22, 30]];
-
-        expect(normal(input)).toEqual([10, 14]);
-      });
-    });
-  });
-
 });
