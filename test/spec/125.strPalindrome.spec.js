325e8d1f8d5f283a59e3a407f099884db1b1cd7e
William He <supervegeter@gmail.com>
2018-07-24 11:26:35 +0800

    revamp 125

--- test/spec/125.strPalindrome.spec.js
+++ test/spec/125.strPalindrome.spec.js
@@ -1,151 +1,54 @@
-import { strPalindrome } from "../../src/125.strPalindrome/solution";
+import * as strPalindrome from "../../src/125.strPalindrome/solution";
 
 describe("# Problem 125 - whether the given string is palindrome. Only consider case-insensitive alphanumeric.", () => {
-  describe("Solution 1: two pointers shrink, with toLowerCase.", () => {
-    it("'' -> true", () => {
-      const result = strPalindrome.twoPointers("");
+  const testSets = [{
+    input: "",
+    output: true
+  }, {
+    input: ";:|{><?",
+    output: true
+  }, {
+    input: "a",
+    output: true
+  }, {
+    input: "aA",
+    output: true
+  }, {
+    input: ".;:|a.;:|{><?a><?",
+    output: true
+  }, {
+    input: "ab",
+    output: false
+  }, {
+    input: "race a car",
+    output: false
+  }, {
+    input: "raceacar",
+    output: false
+  }, {
+    input: "abcdcba",
+    output: true
+  }, {
+    input: "abccdcba",
+    output: false
+  }, {
+    input: "A man, a plan, a canal: Panama",
+    output: true
+  }, {
+    input: ",; W;:GlG:;l ;,",
+    output: false
+  }];
 
-      expect(result).toEqual(true);
-    });
-
-    it("';:|{><?' -> true", () => {
-      const result = strPalindrome.twoPointers(";:|{><?");
-
-      expect(result).toEqual(true);
-    });
-
-    it("'a' -> true", () => {
-      const result = strPalindrome.twoPointers("a");
-
-      expect(result).toEqual(true);
-    });
-
-    it("'aA' -> true", () => {
-      const result = strPalindrome.twoPointers("aA");
-
-      expect(result).toEqual(true);
-    });
-
-    it("'.;:|a.;:|{><?a><?' -> true", () => {
-      const result = strPalindrome.twoPointers(".;:|a.;:|{><?a><?");
-
-      expect(result).toEqual(true);
-    });
-
-    it("'ab' -> false", () => {
-      const result = strPalindrome.twoPointers("ab");
-
-      expect(result).toEqual(false);
-    });
-
-    it("'race a car' -> false", () => {
-      const result = strPalindrome.twoPointers("race a car");
-
-      expect(result).toEqual(false);
-    });
-
-    it("'raceacar' -> false", () => {
-      const result = strPalindrome.twoPointers("raceacar");
-
-      expect(result).toEqual(false);
-    });
-
-    it("'abcdcba' -> true", () => {
-      const result = strPalindrome.twoPointers("abcdcba");
-
-      expect(result).toEqual(true);
-    });
-
-    it("'abccdcba' -> false", () => {
-      const result = strPalindrome.twoPointers("abccdcba");
-
-      expect(result).toEqual(false);
-    });
-
-    it("'A man, a plan, a canal: Panama' -> true", () => {
-      const result = strPalindrome.twoPointers("A man, a plan, a canal: Panama");
-
-      expect(result).toEqual(true);
-    });
-
-    it("',; W;:GlG:;l ;,' -> false", () => {
-      const result = strPalindrome.twoPointers(",; W;:GlG:;l ;,");
-
-      expect(result).toEqual(false);
-    });
-  });
-
-  describe("Solution 2: two pointers shrink, without toLowerCase.", () => {
-    it("'' -> true", () => {
-      const result = strPalindrome.twoPointersInsens("");
-
-      expect(result).toEqual(true);
-    });
-
-    it("';:|{><?' -> true", () => {
-      const result = strPalindrome.twoPointersInsens(";:|{><?");
-
-      expect(result).toEqual(true);
-    });
-
-    it("'a' -> true", () => {
-      const result = strPalindrome.twoPointersInsens("a");
-
-      expect(result).toEqual(true);
-    });
-
-    it("'aA' -> true", () => {
-      const result = strPalindrome.twoPointersInsens("aA");
-
-      expect(result).toEqual(true);
-    });
-
-    it("'.;:|a.;:|{><?a><?' -> true", () => {
-      const result = strPalindrome.twoPointersInsens(".;:|a.;:|{><?a><?");
-
-      expect(result).toEqual(true);
-    });
-
-    it("'ab' -> false", () => {
-      const result = strPalindrome.twoPointersInsens("ab");
-
-      expect(result).toEqual(false);
-    });
-
-    it("'race a car' -> false", () => {
-      const result = strPalindrome.twoPointersInsens("race a car");
-
-      expect(result).toEqual(false);
-    });
-
-    it("'raceacar' -> false", () => {
-      const result = strPalindrome.twoPointersInsens("raceacar");
-
-      expect(result).toEqual(false);
-    });
-
-    it("'abcdcba' -> true", () => {
-      const result = strPalindrome.twoPointersInsens("abcdcba");
-
-      expect(result).toEqual(true);
-    });
-
-    it("'abccdcba' -> false", () => {
-      const result = strPalindrome.twoPointersInsens("abccdcba");
-
-      expect(result).toEqual(false);
-    });
-
-    it("'A man, a plan, a canal: Panama' -> true", () => {
-      const result = strPalindrome.twoPointersInsens("A man, a plan, a canal: Panama");
-
-      expect(result).toEqual(true);
-    });
-
-    it("',; W;:GlG:;l ;,' -> false", () => {
-      const result = strPalindrome.twoPointersInsens(",; W;:GlG:;l ;,");
-
-      expect(result).toEqual(false);
+  Object.entries(strPalindrome).forEach(([name, solution]) => {
+    describe(`Solution: ${name}`, () => {
+      testSets.forEach(({
+        input,
+        output
+      }) => {
+        it(`${input} -> ${output}`, () => {
+          expect(solution(input)).toEqual(output);
+        });
+      });
     });
   });
 });
