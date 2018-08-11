bdd286baffcbd09533446d6425be19ce38c7f5ba
Williammer <supervegeter@gmail.com>
2018-08-11 11:58:51 +0800

    rm helper

--- test/helper.js
+++
@@ -1,7 +0,0 @@
-export const runSuites = (testSuites, solutions) => {
-  testSuites.forEach((t) => {
-    solutions.forEach((solution) => {
-      it(`${t.title} || solution: ${solution}`, t.suite(solution));
-    });
-  });
-};
