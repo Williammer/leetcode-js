7f3c1d882db580b5c606d7d82af31f46f17d0941
Williammer <supervegeter@gmail.com>
2018-08-20 11:13:16 +0800

    refactored 8 atoi

--- src/8.atoi/8.atoi.js
+++ src/8.atoi/8.atoi.js
@@ -1,34 +1,30 @@
+/* eslint-disable no-continue */
 /**
  * Problem: https://leetcode.com/problems/string-to-integer-atoi/
     Implement atoi to convert a string to an integer.
-    Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front.
+    Notes: It is intended for this problem to be specified vaguely (ie, no given input specs).
+    You are responsible to gather all the input requirements up front.
 
  * Examples:
     Please see in spec.js file
-
 
  * @param {string} str
  * @return {number}
 
  * Analysis: we can traverse each char and put into result if valid.
-
  */
 
-export const atoi = {};
-
-atoi.traverse = (str) => {
-  if (str.length === 0) {
+export const traverse = (str) => {
+  if (typeof str !== "string" || str.length === 0) {
     return 0;
   }
 
-  const MAX_INT_32 = Math.pow(2, 31) - 1;
-  let i;
+  const { MAX_INT_32 } = Math;
   let curChar;
   let curCharCode;
-
   let result = 0;
 
-  for (i = 0; i < str.length; i += 1) {
+  for (let i = 0; i < str.length; i += 1) {
     curChar = str[i];
     curCharCode = str.charCodeAt(i);
 
@@ -40,46 +36,39 @@ atoi.traverse = (str) => {
         // skip 0 as 1st digit
         result = curChar * 1;
       }
-    } else {
-      // has something
-      if (typeof result === "number") {
-        if (curCharCode >= 48 && curCharCode <= 57) {
-          // add num to result
-          result = result > 0 ? result * 10 + curChar * 1 : result * 10 - curChar * 1;
-
-          // edge check
-          if (result >= MAX_INT_32) {
-            return MAX_INT_32;
-          }
-          if (result <= -MAX_INT_32 - 1) {
-            return -MAX_INT_32 - 1;
-          }
-        } else {
-          // end
-          break;
-        }
-      } else {
-        // result is "-"
-        if (curCharCode > 48 && curCharCode <= 57) {
-          // add num to "-" and convert it to num
-          result = (result + curChar) * 1;
-        } else if (curChar !== "-" && curCharCode !== 48) {
-          // reset
-          result = 0;
-        }
+      continue;
+    }
+    // has something
+    if (typeof result === "number") {
+      if (!(curCharCode >= 48 && curCharCode <= 57)) {
+        // end
+        break;
       }
+      // add num to result
+      result = result > 0 ? result * 10 + curChar * 1 : result * 10 - curChar * 1;
+
+      if (result >= MAX_INT_32) return MAX_INT_32;
+      if (result <= -MAX_INT_32 - 1) return -MAX_INT_32 - 1;
+      continue;
+    }
+
+    // result is "-"
+    if (curCharCode > 48 && curCharCode <= 57) {
+      // add num to "-" and convert it to num
+      result = (result + curChar) * 1;
+    } else if (curChar !== "-" && curCharCode !== 48) {
+      // reset
+      result = 0;
     }
   }
 
-  if (result === "-") {
-    result = 0;
-  }
+  if (result === "-") result = 0;
 
   return result;
 };
 
 /**
  * Lessons:
-   1. Edge consideration like over/underflow, positive/negative number are the major things to practice here.
-
+   1. Edge consideration like over/underflow, positive/negative number are the major things to
+    practice here.
  */
--- src/8.atoi/8.atoi.spec.js
+++ src/8.atoi/8.atoi.spec.js
@@ -1,37 +1,28 @@
-import { atoi } from "./8.atoi";
+import * as atoi from "./8.atoi";
 
 describe("# Problem 8 - Implement atoi to convert a string to an integer.", () => {
-  describe("Solution 1: traverse string from 1st char to find and convert", () => {
-    it(": '' -> 0", () => {
-      expect(atoi.traverse("")).toEqual(0);
-    });
+  const testCases = [
+    ["", 0],
+    ["-0", 0],
+    ["0b", 0],
+    ["-0b", 0],
+    ["-012", -12],
+    ["12", 12],
+    ["-123", -123],
+    ["123sb", 123],
+    ["  124sb", 124],
+    [" sb   -125", -125],
+    ["sb   2147483647sc346", 2147483647],
+    ["sb   -2147983648sc346", -2147983648],
+  ];
 
-    it(": '12' -> 12", () => {
-      expect(atoi.traverse("12")).toEqual(12);
-    });
-
-    it(": '-123' -> -123", () => {
-      expect(atoi.traverse("-123")).toEqual(-123);
-    });
-
-    it(": '123sb' -> 123", () => {
-      expect(atoi.traverse("123sb")).toEqual(123);
-    });
-
-    it(": '  124sb' -> 124", () => {
-      expect(atoi.traverse("  124sb")).toEqual(124);
-    });
-
-    it(": ' sb   -125' -> -125", () => {
-      expect(atoi.traverse(" sb   -125")).toEqual(-125);
-    });
-
-    it(": 'sb   2147483649sc346' -> 2147483647", () => {
-      expect(atoi.traverse("sb   2147483649sc346")).toEqual(2147483647);
-    });
-
-    it(": 'sb   -2147983648sc346' -> -2147483648", () => {
-      expect(atoi.traverse("sb   -2147983648sc346")).toEqual(-2147483648);
+  Object.keys(atoi).forEach((method) => {
+    describe(`Solution: ${method}`, () => {
+      testCases.forEach(([input, output]) => {
+        it(`"${input}" -> ${output} using ${method}`, () => {
+          expect(atoi[method](input)).toEqual(output);
+        });
+      });
     });
   });
 });
